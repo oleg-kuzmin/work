0f8b2a848e563ddc0827232de17408699eff7844
Oleg Kuzmin <alex-dust@yandex.ru>
2022-10-09 23:32:37 +0300

    array

--- INFO/JAVASCRIPT/МАССИВЫ/array.js
+++ INFO/JAVASCRIPT/МАССИВЫ/array.js
@@ -1,24 +1,3 @@
-// TODO Метод filter. Отсеиванивание элементов массива по признаку.
-const a = [1, 9, 2, 2, 3, 4, 1, 7, 8, 0, 9, 0, 1, 5, 3];
-const b = a.filter(function (item) {
-  // отберём элементы больше 5
-  return item > 5;
-});
-console.log(b); // [9, 7, 8, 9]
-
-const students = [
-  { name: "Oleg", age: "100" },
-  { name: "Vlad", age: "22" },
-  { name: "Elena", age: "20" },
-];
-
-console.log(
-  students
-    .filter((student) => student.age > 20)
-    .map((student) => student.name)
-    .join("\n")
-);
-
 // TODO Метод push. Добавление элемента в конец массива.
 const emeraldCityHeroes = ['Лев', 'Дровосек', 'Страшила'];
 emeraldCityHeroes.push('Элли', 'Тотошка');
---
+++ INFO/JAVASCRIPT/МАССИВЫ/filter.js
@@ -0,0 +1,20 @@
+// TODO Метод filter. Отсеиванивание элементов массива по признаку.
+const a = [1, 9, 2, 2, 3, 4, 1, 7, 8, 0, 9, 0, 1, 5, 3];
+const b = a.filter(function (item) {
+  // отберём элементы больше 5
+  return item > 5;
+});
+console.log(b); // [9, 7, 8, 9]
+
+const students = [
+  { name: "Oleg", age: "100" },
+  { name: "Vlad", age: "22" },
+  { name: "Elena", age: "20" },
+];
+
+console.log(
+  students
+    .filter((student) => student.age > 20)
+    .map((student) => student.name)
+    .join("\n")
+);
