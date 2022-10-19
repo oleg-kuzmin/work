c15a0be5aa63661c47ad553afdcaa176e6ea81f9
Oleg Kuzmin <alex-dust@yandex.ru>
2022-10-19 22:22:15 +0300

    js

--- INFO/JAVASCRIPT/DOM.js
+++ INFO/JAVASCRIPT/DOM.js
@@ -1,5 +1,7 @@
 const headingElement = document.querySelector("h1"); // поиск по тегу
-const bodyElem = document.querySelector("body"); // поиск по классу
+const bodyElem = document.querySelector(".body"); // поиск по классу
+const button = document.querySelector('#button'); // поиск по id
+const button1 = document.querySelectorAll('.button'); // найдет все по классу
 bodyElem.style.backgroundColor = "gray"; // синтаксис camelCase для стилей
 headingElement.style.fontSize = "40px";
 headingElement.style.color = "white";
