73ee539b1f995b2dd6c40f02e889388a43e91b58
Oleg Kuzmin <alex-dust@yandex.ru>
2022-10-19 22:20:53 +0300

    js

--- INFO/JAVASCRIPT/МАССИВЫ/Массивы.js
+++ INFO/JAVASCRIPT/МАССИВЫ/Массивы.js
@@ -3,3 +3,6 @@ const numbers = [123, 134, 1223, 552];
 lastElem = numbers.at(1);
 console.log(lastElem);
 
+//СВОЙСТВО ДЛИНА
+nameUser.length; // определяет длину элемента (или массива)
+'котики'.length // 5
