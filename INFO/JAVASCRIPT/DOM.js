ed75dab0d649e6aed5f0b3c309374a85eb272232
Oleg Kuzmin <alex-dust@yandex.ru>
2022-10-19 22:23:43 +0300

    js

--- INFO/JAVASCRIPT/DOM.js
+++ INFO/JAVASCRIPT/DOM.js
@@ -7,3 +7,14 @@ headingElement.style.fontSize = "40px";
 headingElement.style.color = "white";
 headingElement.textContent = "текст";
 headingElement.append("новый текст");
+
+element.insertAdjacentHTML('beforebegin', '***') // гибкая вставка до открытия тега ! не использовать из-за безопасности !
+// <div class="***">
+      element.insertAdjacentHTML('afterbegin', '***') // гибкая вставка перед разметкой ! не использовать из-за безопасности !
+      // текущая разметка
+      element.insertAdjacentHTML('beforeend', '***') // гибкая вставка после разметки ! не использовать из-за безопасности !
+// </div>
+element.insertAdjacentHTML('afterend', '***') // гибкая вставка после закрытия тега ! не использовать из-за безопасности !
+
+element.textContent // получим текстовое содержимое тега, можно перезаписать ! а вот это можно и нужно использовать !
+element.insertAdjacentText // гибкая вставка текстового содержимого тега, работает так же как insertAdjacentHTML ! а вот это можно и нужно использовать !
