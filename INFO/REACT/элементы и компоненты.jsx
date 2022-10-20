62e5851149f1b31c79539f051fc3e109d3747d9f
Oleg Kuzmin <alex-dust@yandex.ru>
2022-10-20 12:01:32 +0300

    react

--- INFO/REACT/элементы и компоненты.jsx
+++ INFO/REACT/элементы и компоненты.jsx
@@ -30,3 +30,36 @@ ReactDOM.render(
   element,
   document.querySelector('#root')
 );
+
+// Еще пример
+class Player extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <li className="table__text">{this.props.children}</li>;
+  }
+}
+
+class List extends React.Component {
+  render() {
+    return (
+      <section className="table">
+        <h1 className="table__title">
+          Игроки киберспортивной команды <span className="table__span">NaVi</span>
+        </h1>
+        <ol className="table__content">
+          <Player children="b1t" />
+          <Player children="s1mple" />
+          <Player children="electronic" />
+          <Player children="sdy" />
+          <Player children="Perfecto" />
+        </ol>
+      </section>
+    );
+  }
+}
+
+ReactDOM.render(<List />, document.querySelector('#team-container'));
+
