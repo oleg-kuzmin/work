578026862d2e01d765bfbaaf1764affb93deb28c
Oleg Kuzmin <alex-dust@yandex.ru>
2022-10-28 19:48:38 +0300

    onChange react

--- INFO/REACT/onChange.jsx
+++ INFO/REACT/onChange.jsx
@@ -1,7 +1,8 @@
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+import React, { useState } from "react";
 
 function App() {
   let [likes, setlikes] = useState(5);
+  let [value, setValue] = useState("ТЕКСТ В ИНПУТЕ");
 
   function increment() {
     setlikes((likes += 1));
@@ -14,14 +15,12 @@ function App() {
   return (
     <div>
       <h1>{likes}</h1>
+      <p>{value}</p>
+      <input type="text" value={value} onChange={(event) => setValue(event.target.value)} />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default App;
