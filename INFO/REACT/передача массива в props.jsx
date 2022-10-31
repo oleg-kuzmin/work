f48ff3956c487cf1a3321349eaa6a42bc2614350
Oleg Kuzmin <alex-dust@yandex.ru>
2022-10-31 15:23:09 +0300

    react

---
+++ INFO/REACT/передача массива в props.jsx
@@ -0,0 +1,16 @@
+const { useState } = require("react");
+
+function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "First Cat" },
+    { id: 2, title: "Second Cat" },
+  ]);
+
+  return (
+    <div className="App">
+      {posts.map((post) => (
+        <Postitem post={post} />
+      ))}
+    </div>
+  );
+}
