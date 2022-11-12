cca6bea2c78d374fb65e03e93fcc9c017d61dcd4
Oleg Kuzmin <alex-dust@yandex.ru>
2022-11-12 14:01:39 +0300

    react

---
+++ INFO/REACT/09. формы.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+    };
+    this.handleCnange = this.handleCnange.bind(this);
+  }
+
+  handleCnange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="First Name"
+          onChange={this.handleCnange}
+          name="firstName"
+          value={this.state.firstName}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={this.handleCnange}
+          name="lastName"
+          value={this.state.lastName}
+        />
+        <h1>
+          {this.state.firstName} {this.state.lastName}
+        </h1>
+      </form>
+    );
+  }
+}
+
+export default App;
