bfa10517c66e88a6a8eb595b7a913490668150c5
Oleg Kuzmin <alex-dust@yandex.ru>
2022-11-29 17:15:01 +0300

    js

---
+++ INFO/JAVASCRIPT/querySelector.js
@@ -0,0 +1,5 @@
+// Поиск элемента в html
+
+element.querySelector("#container"); // по индификатору
+element.querySelector(".content"); // по имени класса
+element.querySelectorAll(".content__item"); //все
