d7cbeb38644ebe8ef0b1f202f03ce7299f7b0cc4
Oleg Kuzmin <alex-dust@yandex.ru>
2022-12-01 22:16:45 +0300

    js

--- INFO/JAVASCRIPT/МАССИВЫ/forEach (функция для каждого элемента).js
+++ INFO/JAVASCRIPT/МАССИВЫ/forEach (функция для каждого элемента).js
@@ -1,9 +1,9 @@
 // TODO Метод forEach. Функция для каждого элемента массива (ничего не возвращает)
 
 //* Синтаксис
-arr.forEach(function(item, index, arr) {})
+arr.forEach(function (item, index, arr) {});
 
-const how = ['быстрее', 'выше', 'сильнее'];
+const how = ["быстрее", "выше", "сильнее"];
 how.forEach(function (item) {
-  console.log(item + '.'); // быстрее. выше. сильнее.
+  console.log(item + "."); // быстрее. выше. сильнее.
 });
--- INFO/JAVASCRIPT/МАССИВЫ/map (создание нового массива).js
+++ INFO/JAVASCRIPT/МАССИВЫ/map (создание нового массива).js
@@ -1,7 +1,7 @@
 // TODO Метод map. Создание одного массива из другого. Вызывает функцию для каждого элемента массива и возвращает новый массив из результатов
 
 //* Синтаксис
-arr.map(function(item, index, arr) {});
+arr.map(function (item, index, arr) {});
 
 const firstArr = [0, 1, 2, 3, 4];
 const secondArr = firstArr.map(function (item) {
@@ -11,6 +11,4 @@ const secondArr = firstArr.map(function (item) {
 console.log(secondArr); // [0, 1, 4, 9, 16]
 
 const studentsName = ["Oleg", "Vlad", "Elena"];
-console.log(
-  studentsName.map((name, index) => `${index + 1}. ${name}`).join("\n")
-);
+console.log(studentsName.map((name, index) => `${index + 1}. ${name}`).join("\n"));
