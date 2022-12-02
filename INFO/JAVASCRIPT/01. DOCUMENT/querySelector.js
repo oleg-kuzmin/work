27ed99bc538f91cfc3b912d9027defc972a483eb
Oleg Kuzmin <alex-dust@yandex.ru>
2022-12-02 15:30:35 +0300

    js

---
+++ INFO/JAVASCRIPT/01. DOCUMENT/querySelector.js
@@ -0,0 +1,5 @@
+// Поиск элемента в html
+
+document.querySelector("#container"); // по индификатору
+document.querySelector(".content"); // по имени класса
+document.querySelectorAll(".content__item"); //все
--- INFO/JAVASCRIPT/querySelector.js
+++
@@ -1,5 +0,0 @@
-// Поиск элемента в html
-
-element.querySelector("#container"); // по индификатору
-element.querySelector(".content"); // по имени класса
-element.querySelectorAll(".content__item"); //все
