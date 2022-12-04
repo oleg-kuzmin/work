ecbf2d625456642fc1d91e5416a85db9781048c5
Oleg Kuzmin <alex-dust@yandex.ru>
2022-12-04 23:17:51 +0300

    js

--- INFO/JAVASCRIPT/МАССИВЫ/shift (удаляет первый элемент из массива, возвращает последний).js
+++ INFO/JAVASCRIPT/МАССИВЫ/shift (удаляет первый элемент из массива, возвращает последний).js
@@ -1,4 +1,4 @@
-// TODO Метод shift. Удаление первого элемента массива.
+// TODO Метод shift. Удаляет первый элемент из массива (изменяя его) и возвращает последний элемент массива
 const italyCities = ['Помпеи', 'Рим', 'Неаполь'];
 const volcanoEruption = italyCities.shift();
 console.log(volcanoEruption); // "Помпеи" метод shift() возвращает удалённый элемент
