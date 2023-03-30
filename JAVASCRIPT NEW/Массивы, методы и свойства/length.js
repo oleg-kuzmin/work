cda615c5b1b719dc397b639d395edbeefe11742d
Oleg Kuzmin <alex-dust@yandex.ru>
2023-03-30 14:11:15 +0300

    js

--- JAVASCRIPT NEW/Массивы, методы и свойства/length.js
+++ JAVASCRIPT NEW/Массивы, методы и свойства/length.js
@@ -1,4 +1,6 @@
 //# length
 // Массивы хранят свой размер в свойстве length:
-const infoArraay = ['Россия', 'Москва', 144.5, 'Russian ruble', true];
+
+//# пример
+const infoArray = ['Россия', 'Москва', 144.5, 'Russian ruble', true];
 console.log(infoArray.length); // 5
