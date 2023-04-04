3d3214d1eeb8cee41a4de92c0cb28cf8dd479203
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-04 19:28:43 +0300

    js

---
+++ JAVASCRIPT NEW/Строки, методы и свойства/toLowerCase.js
@@ -0,0 +1,6 @@
+//# .toLowerCase()
+// Используется для приведения всех символов строки к нижнему регистру.
+
+//# пример
+const lowercased = 'СОБАКА'.toLowerCase();
+console.log(lowercased); // собака
---
+++ JAVASCRIPT NEW/Строки, методы и свойства/toUpperCase.js
@@ -0,0 +1,6 @@
+//# .toUpperCase()
+// Используется для приведения всех символов строки к верхнему регистру.
+
+//# пример
+const uppercased = 'котик'.toUpperCase();
+console.log(uppercased); // КОТИК
