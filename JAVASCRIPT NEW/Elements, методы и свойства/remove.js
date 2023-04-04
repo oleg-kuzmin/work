7e6a92d84a34aca2f2c3b01c949461a9b63a5bff
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-04 17:06:25 +0300

    js

--- JAVASCRIPT NEW/Elements, методы и свойства/remove.js
+++ JAVASCRIPT NEW/Elements, методы и свойства/remove.js
@@ -1,5 +1,4 @@
 //# .remove()
 // Если нужно удалить элемент и все вложенные в него, воспользуйтесь методом remove():
-
 const element = document.querySelector("#some-element");
 element.remove();
---
+++ JAVASCRIPT NEW/Elements, методы и свойства/value.js
@@ -0,0 +1,5 @@
+//# .value
+// Значение поля ввода. Оно есть у всех элементов input.
+let inputs = document.querySelectorAll('input');
+console.log(inputs[0].value); // "Ты опоздала"
+console.log(inputs[1].value); // "Игорь Тальков"
