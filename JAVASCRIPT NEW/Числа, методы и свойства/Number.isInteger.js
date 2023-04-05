fe68a7e654074143ee4b1751db4f9748fe2ee54f
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-05 18:35:12 +0300

    js

---
+++ JAVASCRIPT NEW/Числа, методы и свойства/Number.isInteger.js
@@ -0,0 +1,7 @@
+//# Number.isInteger()
+// Метод Number.isInteger принимает число как аргумент и проверяет, целое оно или дробное.
+
+//# пример
+const eightAndAHalf = 8.5;
+Number.isInteger(eightAndAHalf); // false
+Number.isInteger(Math.floor(eightAndAHalf)); // true
