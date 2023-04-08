9bd8c798af942592a7eb2689e03d64b59fd67d4f
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-08 14:03:03 +0300

    js

--- JAVASCRIPT NEW/Elements, методы и свойства/nextElementSibling.js
+++ JAVASCRIPT NEW/Elements, методы и свойства/nextElementSibling.js
@@ -1,3 +1,9 @@
 //# .nextElementSibling
-// Получить следующий узел-сосед.
-element.nextElementSibling;
+// Свойство previousElementSibling содержит ссылку на следующий соседний элемент.
+
+//# пример
+const element = document.querySelectorAll('p')[1];
+console.log(element.nextElementSibling); // <p>Ребёнок три</p>
+
+//# общая информация
+// Если у элемента нет соседа, вернется null. Все эти свойства доступны только для чтения. Перезаписать их не получится.
--- JAVASCRIPT NEW/Elements, методы и свойства/previousElementSibling.js
+++ JAVASCRIPT NEW/Elements, методы и свойства/previousElementSibling.js
@@ -1,3 +1,9 @@
 //# .previousElementSibling
-// Получить предыдущий узел-сосед.
-element.previousElementSibling;
+// Свойство previousElementSibling содержит ссылку на предыдущий соседний элемент.
+
+//# пример
+const element = document.querySelectorAll('p')[1];
+console.log(element.previousElementSibling); // <p>Ребёнок раз</p>
+
+//# общая информация
+// Если у элемента нет соседа, вернется null. Все эти свойства доступны только для чтения. Перезаписать их не получится.
