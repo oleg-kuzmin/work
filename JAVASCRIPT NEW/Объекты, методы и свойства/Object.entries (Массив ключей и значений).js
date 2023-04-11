6467b121fb4a3e8ca00ce020313b769510238b65
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-11 17:48:13 +0300

    js

---
+++ JAVASCRIPT NEW/Объекты, методы и свойства/Object.entries (Массив ключей и значений).js
@@ -0,0 +1,9 @@
+//# Object.entries()
+// Метод возвращает все пары «ключ-значение».
+
+//# пример
+const paulMcCartney = {
+  yesterday: 'all my troubles seemed so far away',
+};
+
+console.log(Object.entries(paulMcCartney)); // [["yesterday", "all my troubles seemed so far away"]]
---
+++ JAVASCRIPT NEW/Объекты, методы и свойства/Object.values (Массив значений).js
@@ -0,0 +1,9 @@
+//# Object.values()
+// Возвращает значения всех свойств и методов объекта
+
+//# пример
+const paulMcCartney = {
+  yesterday: 'all my troubles seemed so far away'
+};
+
+console.log( Object.values(paulMcCartney) ); // ["all my troubles seemed so far away"]
