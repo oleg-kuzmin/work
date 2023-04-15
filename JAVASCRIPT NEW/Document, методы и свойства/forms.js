7215137e3a18461f4fc0dbec53136110b0ad6d28
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-15 15:16:55 +0300

    js

--- JAVASCRIPT NEW/Document, методы и свойства/forms.js
+++ JAVASCRIPT NEW/Document, методы и свойства/forms.js
@@ -1,5 +1,8 @@
 //# .forms
-// Получить список форм на странице. Свойство только для чтения, напрямую перезаписать его нельзя.
+// Получить список форм (псевдомассив) на странице. Свойство только для чтения, напрямую перезаписать его нельзя.
 
 //# пример
-const collection = document.forms;
+const collection = document.forms; // псевдомассив форм
+const myForm = document.forms.formName; // поиск по имени
+const element = document.forms.formName.element; // элемент формы
+
