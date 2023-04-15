5e5b6698644927c18c0542cc0125780b3b99835e
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-15 14:48:40 +0300

    js

--- JAVASCRIPT NEW/Document, методы и свойства/window.js
+++ JAVASCRIPT NEW/Document, методы и свойства/window.js
@@ -1,3 +1,5 @@
 //# window
 // Объект window - это окно браузера, а document - это содержимое всего сайта (корень документа).
 window.document;
+
+// window - document - html - body
--- JAVASCRIPT NEW/События/evt.js
+++ JAVASCRIPT NEW/События/evt.js
@@ -5,8 +5,20 @@ button.addEventListener('click', function (evt) {
   eventTarget.setAttribute('disabled', true);
 });
 
+//* evt.preventDefault();
+// отмена стандартного события
+
 //* evt.target
-// хранится элемент, на котором сработало событие
+// хранит элемент, на котором сработало событие (самый глубокий элемент DOM-дерева из всех, где сработало событие)
+playlist.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains('song__like')) {
+    like(evt.target);
+  }
+});
+
+//* evt.currentTarget
+// evt.target хранит элемент, где возникло событие
+// evt.currentTarget — элемент, где сработал обработчик
 
 //* evt.key
 // хранит название нажатой клавиши
@@ -15,11 +27,24 @@ button.addEventListener('click', function (evt) {
 // хранит уникальный код нажатой клавиши
 // по новым стандартам устарел, браузеры могут перестать его поддерживать
 
-//* event.pageX и event.pageY
+//* evt.pageX и evt.pageY
 // координаты по горизонтали и вертикали от угла веб-страницы
 
-//* event.clientX и event.clientY
+//* evt.clientX и evt.clientY
 // координаты относительно угла окна просмотра
 
-//* event.screenX и event.screenY
+//* evt.screenX и evt.screenY
 // координаты относительно угла монитора, на котором открыт сайт. Они используются крайне редко
+
+//* evt.stopPropagation()
+// Метод stopPropagation останавливает всплытие событий на элементе (дальше элемента всплытие не пойдет)
+function callback(evt) {
+  evt.stopPropagation();
+}
+
+//* event.stopImmediatePropagation()
+// отменяет не только всплытие событий, но и срабатывание всех других обработчиков того же события на этом элементе.
+// остановит только те обработчики, которые описаны после него. Он попросту не знает о тех, которые стоят в коде раньше.
+element.addEventListener('click', function (event) {
+  event.stopImmediatePropagation();
+});
