34bc1c1d9954217703f0561a4fd88f0198439b67
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-17 18:59:20 +0300

    js

--- JAVASCRIPT NEW/Модули/Импорт в модуль.js
+++ JAVASCRIPT NEW/Модули/Импорт в модуль.js
@@ -1,5 +1,6 @@
 //# Импорт в модуль
-import { str, myFunc } from './script-01.js';
+import { str } from './lib.js'; // нужно использовать как объект {}
+import { str, myFunc } from './script-01.js'; // несколько элементов из одного файла
 
 console.log(str); // "Я переменная из модуля script-01.js"
 myFunc(); // "Я функция из модуля script-01.js"
