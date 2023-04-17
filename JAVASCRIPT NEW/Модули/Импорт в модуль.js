8e043d0c2ed659e8a84b8adc3f9daf61d7fe74b4
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-17 18:54:45 +0300

    js

---
+++ JAVASCRIPT NEW/Модули/Импорт в модуль.js
@@ -0,0 +1,5 @@
+//# Импорт в модуль
+import { str, myFunc } from './script-01.js';
+
+console.log(str); // "Я переменная из модуля script-01.js"
+myFunc(); // "Я функция из модуля script-01.js"
---
+++ JAVASCRIPT NEW/Модули/Экспорт из модуля.js
@@ -0,0 +1,6 @@
+//# Экспорт из модуля
+export const str = 'Я переменная из модуля script-01.js';
+
+export function myFunc() {
+  console.log('Я функция из модуля script-01.js');
+}
