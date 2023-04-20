86c43a96ba848c2b14d82713c2322a1528a702e5
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-20 19:30:35 +0300

    npm

--- WEBPACK/09. webpack.config.js
+++ WEBPACK/09. webpack.config.js
@@ -1,20 +1,22 @@
 const path = require('path'); // подключаем path к конфигу вебпак (утилита, которая превращает относительный путь в абсолютный)
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключаем плагин html-webpack-plugin (для работы с html)
 
 // module.exports — это синтаксис экспорта в Node.js
 module.exports = {
-  entry: { main: './src/index.js' }, // указали первое место, куда заглянет webpack, — файл index.js в папке src
+  //# точка входа — файл index.js в папке src
+  entry: { main: './src/index.js' },
 
-  // объект output (точка выхода) - итоговый файл, куда «Вебпак» сложит весь js-код
+  //# объект output (точка выхода) - итоговый файл, куда «Вебпак» сложит весь js-код
   output: {
     path: path.resolve(__dirname, 'dist'), // абсолютный путь к точке выхода
     filename: 'main.js', // имя файла, куда «Вебпак» положит код
     publicPath: '', // свойство для обновления путей внутри CSS- и HTML-файлов
   },
 
-  // режим разработчика
+  //# режим разработчика
   mode: 'development',
 
-  // настройки локального сервера
+  //# настройки локального сервера
   devServer: {
     static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
     compress: true, // это ускорит загрузку в режиме разработки
@@ -22,18 +24,23 @@ module.exports = {
     open: true, // сайт будет открываться сам при запуске npm run dev
   },
 
-  // файлы нужно пропускать через Babel (наше правило звучит так: «если тебе попадётся файл с расширением .js, сначала отдай этот файл модулю babel-loader, а затем добавляй в сборку. Но не применяй это правило к пакетам, скачанным из NPM, которые лежат в папке node_modules»)
+  //# настройка Babel
   module: {
     // rules — это массив правил, добавим в него объект правил для бабеля
     rules: [
       {
-        // регулярное выражение, которое ищет все js файлы
-        test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
-        use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: '/node_modules/',
+        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+        use: 'babel-loader', // при обработке этих файлов нужно использовать babel-loader
+        exclude: '/node_modules/', // исключает папку node_modules, файлы в ней обрабатывать не нужно
       },
     ],
   },
+
+  //# plugins - массив плагинов
+  plugins: [
+    //# настройка html-webpack-plugin
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // путь к файлу index.html
+    }),
+  ],
 };
