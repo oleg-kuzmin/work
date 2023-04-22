615fd029d3aa56e277c30c116db52c64caabea06
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-22 16:36:52 +0300

    js

--- JAVASCRIPT NEW/Асинхронность/Колбеки - пример работы тега img.js
+++ JAVASCRIPT NEW/Асинхронность/Колбеки - пример работы тега img.js
@@ -7,11 +7,17 @@ function imageLoadCallback(evt) {
   document.body.append(evt.target); // после загрузки добавим элемент изображения в DOM
 }
 
+//* колбэк, который нужно выполнить в случае ошибки
+function handleLoadError() {
+  console.log('Всё идёт не по плану');
+}
+
 //* Функция для создания изображения
-function loadImage(imageUrl, loadCallback) {
+function loadImage(imageUrl, loadCallback, errorCallback) {
   const img = document.createElement('img');
   img.src = imageUrl;
   img.onload = loadCallback; // функция, которая записана в onload будет вызвана после загрузки изображения
+  img.onerror = errorCallback; // функция, которая записана в onerror будет вызвана в случае ошибки
 }
 
 //* Теперь картинка появится в разметке только после загрузки
