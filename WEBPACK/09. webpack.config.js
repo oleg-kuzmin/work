34c2179aa61ddc471ba5ca1f140d032e0ddf6533
Oleg Kuzmin <alex-dust@yandex.ru>
2023-04-30 10:05:54 +0300

    webpack

--- WEBPACK/09. webpack.config.js
+++ WEBPACK/09. webpack.config.js
@@ -20,10 +20,11 @@ module.exports = {
 
   //# настройки локального сервера
   devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    watchFiles: path.join(__dirname, 'src'), // путь, куда "смотрит" режим разработчика
     compress: true, // это ускорит загрузку в режиме разработки
     port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
     open: true, // сайт будет открываться сам при запуске npm run dev
+    // static: path.resolve(__dirname, './dist'), // старое
   },
 
   //# настройка Babel
