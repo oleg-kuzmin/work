b66c6570ac323a1f65ed9fa1e2fd30b24a6705ee
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-03 10:43:21 +0300

    webpack

--- WEBPACK/09. webpack.config.js
+++ WEBPACK/09. webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
 
   //# настройки локального сервера
   devServer: {
-    watchFiles: path.join(__dirname, 'src'), // путь, куда "смотрит" режим разработчика
+    watchFiles: path.join(__dirname, 'src'), // смотрит изменения файлов в папке
     compress: true, // это ускорит загрузку в режиме разработки
     port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
     open: true, // сайт будет открываться сам при запуске npm run dev
