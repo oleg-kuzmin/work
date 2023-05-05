e45060984ea3b39162b15975b4a27c5627699111
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-05 14:01:31 +0300

    js

--- JAVASCRIPT NEW/Циклы/04. for..of.js
+++ JAVASCRIPT NEW/Циклы/04. for..of.js
@@ -8,21 +8,23 @@ for (let a of arr) {
 }
 
 //# общая информация
-/* Примеры итерируемых объектов:
+/*
+Примеры итерируемых объектов:
 1) String
 2) Map
 3) TypedArray
 4) Array
 5) Set
-6) Generator */
+6) Generator
+*/
 
 //# способ проверить, является ли объект итерируемым
 //* является
-const str = new String("Chidume");
+const str = new String('Chidume');
 log(typeof str[Symbol.iterator]); // function
 
 //* не является
 const obj = {
-  surname: "Chidume",
+  surname: 'Chidume',
 };
 log(typeof obj[Symbol.iterator]); // undefined
