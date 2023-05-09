8d4205605ff5389ec384d479d89dad0c1f0f27e9
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-09 12:59:19 +0300

    js

--- JAVASCRIPT NEW/Фишки/JS - Валидация всех форм c кастомным сообщением.js
+++ JAVASCRIPT NEW/Фишки/JS - Валидация всех форм c кастомным сообщением.js
@@ -1,19 +1,3 @@
-/*
-1. Сделать пустой span (в него будем добавлять текст из validationMessage)
-2. Добавить id/name для input (id="email-input") или (name="email-input")
-3. Добавить уникальный class для span (class="email-input-error)
-*/
-
-//# html
-<form class="form" novalidate>
-  <label class="form__field">
-    Введите адрес электронной почты
-    <input id="email-input" class="form__input" type="email" placeholder="Email" required />
-    <span class="email-input-error form__input-error"></span>
-  </label>
-  <button class="form__submit">Войти</button>
-</form>;
-
 //# настройка валидации
 const validateConfig = {
   buttonSubmit: '.popup__button-save',
@@ -21,28 +5,24 @@ const validateConfig = {
   input: '.popup__input',
   inputInvalid: 'popup__input_type_error',
   spanInvalid: 'popup__input-error_active',
+  form: '.popup__form',
   pattern: /^([а-яА-ЯёЁa-zA-Z]|\s|-|\n)+$/,
   patternMessage: 'Допустимы только латинские буквы, кириллические буквы, знаки дефиса или пробелы',
 };
 
-//# 1 — Функция, которая найдет и переберет все формы на странице
+//# 1. Функция, которая найдет и переберет все формы на странице
 const enableValidation = () => {
   //* найдём все формы, сделаем из них массив
   const formList = Array.from(document.forms);
 
-  //* Переберём полученную коллекцию
-  formList.forEach((form) => {
-    form.addEventListener('submit', function (evt) {
-      evt.preventDefault();
-    });
-
-    //* вызовем функцию setEventListeners, передав ей элемент формы
+  //* Переберём полученную коллекцию и вызовем функцию setEventListeners, передав ей элемент формы
+  formList.forEach(form => {
     setEventListeners(form);
   });
 };
 
-//# 2 — Функция, которая примет параметром элемент формы и добавит её полям нужные обработчики
-const setEventListeners = (form) => {
+//# 2. Функция, которая примет параметром элемент формы и добавит её полям нужные обработчики
+const setEventListeners = form => {
   //* находим все поля input внутри формы с указанным классом, сделаем из них массив
   const inputList = Array.from(form.querySelectorAll(validateConfig.input));
 
@@ -52,8 +32,8 @@ const setEventListeners = (form) => {
   //* проверим состояние кнопки submit
   checkButtonSubmit(inputList, buttonSubmit);
 
-  //* обойдём все элементы массива input и каждому добавим обработчик события
-  inputList.forEach((input) => {
+  //* обойдём все элементы массива input и каждому добавим обработчик
+  inputList.forEach(input => {
     input.addEventListener('input', () => {
       checkInputValidity(form, input);
       checkButtonSubmit(inputList, buttonSubmit);
@@ -61,65 +41,73 @@ const setEventListeners = (form) => {
   });
 };
 
-//# 3 - Функция принимает массив полей ввода и элемент кнопки, состояние которой нужно менять
+//# 3. Функция принимает массив полей ввода и элемент кнопки, состояние которой нужно менять
 const checkButtonSubmit = (inputList, buttonSubmit) => {
   if (hasInvalidInput(inputList)) {
-    // disableButtonSubmit(buttonSubmit);
-    buttonSubmit.disabled = true;
-    buttonSubmit.classList.add(validateConfig.buttonSubmitInvalid);
+    disableButtonSubmit(buttonSubmit);
   } else {
-    // enableButtonSubmit(buttonSubmit);
-    buttonSubmit.disabled = false;
-    buttonSubmit.classList.remove(validateConfig.buttonSubmitInvalid);
+    enableButtonSubmit(buttonSubmit);
   }
 };
 
-//# Функция выключает кнопку
-const disableButtonSubmit = (button) => {
+//# 4. Функция выключает кнопку
+const disableButtonSubmit = button => {
   button.disabled = true;
   button.classList.add(validateConfig.buttonSubmitInvalid);
 };
 
-//# Функция включает кнопку
-const enableButtonSubmit = (button) => {
+//# 5. Функция включает кнопку
+const enableButtonSubmit = button => {
   button.disabled = false;
   button.classList.remove(validateConfig.buttonSubmitInvalid);
 };
 
-//# 4 - Функция, которая проверяет валидность массива всех полей
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    if (!inputElement.validity.valid || !validateConfig.pattern.test(inputElement.value)) {
+//# 6. Функция, которая проверяет валидность массива всех полей
+const hasInvalidInput = inputList => {
+  return inputList.some(inputElement => {
+    if (!inputElement.validity.valid || (!validateConfig.pattern.test(inputElement.value) && inputElement.type !== 'url')) {
       return true;
     } else return false;
   });
 };
 
-//# 5 — Функция, которая проверяет валидность поля
+//# 7. Функция, которая проверяет валидность поля
 const checkInputValidity = (form, input) => {
   if (!input.validity.valid) {
     showInputError(form, input, input.validationMessage);
-  } else if (!validateConfig.pattern.test(input.value)) {
+  } else if (!validateConfig.pattern.test(input.value) && input.type !== 'url') {
     showInputError(form, input, validateConfig.patternMessage);
   } else {
     hideInputError(form, input);
   }
 };
 
-//# 6 — Функция, которая добавляет класс с ошибкой
+//# 8. Функция, которая добавляет класс с ошибкой
 const showInputError = (form, input, errorMessage) => {
-  const span = form.querySelector(`.${input.id}-error`);
+  const span = form.querySelector(`.${input.name}-error`);
   input.classList.add(validateConfig.inputInvalid);
   span.textContent = errorMessage;
   span.classList.add(validateConfig.spanInvalid);
 };
 
-//# 7 — Функция, которая удаляет класс с ошибкой
+//# 9. Функция, которая удаляет класс с ошибкой
 const hideInputError = (form, input) => {
-  const span = form.querySelector(`.${input.id}-error`);
+  const span = form.querySelector(`.${input.name}-error`);
   input.classList.remove(validateConfig.inputInvalid);
-  span.classList.remove(validateConfig.spanInvalid);
   span.textContent = '';
+  span.classList.remove(validateConfig.spanInvalid);
 };
 
-export { enableValidation };
+//# 10. Функция, сброс по умолчанию
+const resetForm = node => {
+  const currentSubmitButton = node.querySelector(validateConfig.buttonSubmit);
+  const currentForm = node.querySelector(validateConfig.form);
+  const currentInputList = Array.from(currentForm.querySelectorAll(validateConfig.input));
+  disableButtonSubmit(currentSubmitButton);
+  currentInputList.forEach(input => {
+    hideInputError(currentForm, input);
+  });
+  currentForm.reset();
+};
+
+export { enableValidation, resetForm };
