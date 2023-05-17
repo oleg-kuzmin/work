8a25050ea63fb5ee63d467468ac5de53bc299ab0
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-17 10:34:33 +0300

    js

--- JAVASCRIPT/ГЛОБАЛЬНЫЕ КЛАССЫ/new Date.js
+++ JAVASCRIPT/ГЛОБАЛЬНЫЕ КЛАССЫ/new Date.js
@@ -1,5 +1,21 @@
-// время часы
-new Date().getHours();
+const currentDate = new Date();
 
-// время до секунды
+// часы
+new Date().getHours();
+currentDate.getHours();
+
+// секунды
 new Date().toLocaleTimeString();
+currentDate.toLocaleTimeString();
+
+// день
+new Date().getDate();
+currentDate.getDate();
+
+// месяц
+new Date().getMonth();
+currentDate.getMonth();
+
+// год
+new Date().getFullYear();
+currentDate.getFullYear();
