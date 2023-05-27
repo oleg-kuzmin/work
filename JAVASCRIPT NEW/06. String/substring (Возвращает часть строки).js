7cac915d417194953dbd2b0a8c4432d3d035fd08
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-27 17:19:42 +0300

    js

---
+++ JAVASCRIPT NEW/06. String/substring (Возвращает часть строки).js
@@ -0,0 +1,23 @@
+//# .substring() (Возвращает часть строки).js
+/*
+- Самый удобный способ получить подстроку — это метод substring().
+- Метод substring() копирует указанную часть строки и возвращает копию в качестве результата.
+- Метод принимает один или два аргумента.
+- При вызове с двумя аргументами нужно передать индекс символа, с которого начинать копирование и индекс символа, на котором закончить.
+- Индекс окончания не включается в копию.
+*/
+
+//# пример
+const phrase = 'javascript';
+const substring = phrase.substring(4, 10);
+console.log(substring); // script
+console.log(phrase.substring(4, 9)); // scrip
+
+//* если указан только один аргумент, то результатом будет строка, начинающаяся с указанного индекса и до конца строки
+const phrase2 = 'javascript';
+console.log(phrase2.substring(0, 4)); // java
+console.log(phrase2.substring(1)); // avascript
+console.log(phrase2.substring(4)); // script
+
+//* поменяв местами индекс старта и индекс окончания копирования substring() поймёт, что копировать
+console.log(phrase2.substring(4, 0)); // java
