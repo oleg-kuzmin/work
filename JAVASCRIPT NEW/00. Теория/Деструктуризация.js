94bba981ee9992ee928d4a1e4653cf5d35c1b6d8
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-28 15:05:44 +0300

    js

---
+++ JAVASCRIPT NEW/00. Теория/Деструктуризация.js
@@ -0,0 +1,6 @@
+//# деструктуризация массива
+const catProfile = ['Maru', 'Scottish Fold', true, ''];
+const [name, breed] = catProfile;
+
+console.log(name); // Maru
+console.log(breed); // Scottish Fold
---
+++ JAVASCRIPT NEW/07. Array/Array.from (Cоздаёт массив из псевдомассива).js
@@ -0,0 +1,4 @@
+//# Array.from() (Cоздаёт массив из псевдомассива)
+
+const posts = content.querySelectorAll('.post');
+const postsArray = Array.from(posts);
