b2dbd9edea5067a394c23f1184ce7ed67a4a9027
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-29 10:21:42 +0300

    js

---
+++ JAVASCRIPT NEW/00. Теория/ЗЗамыкания.js
@@ -0,0 +1,16 @@
+function Counter() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+const counter = Counter();
+console.log(counter()); // 1
+console.log(counter()); // 2
+console.log(counter()); // 3
+
+const anotherCounter = Counter();
+console.log(anotherCounter()); // 1
+console.log(anotherCounter()); // 2
+console.log(anotherCounter()); // 3
