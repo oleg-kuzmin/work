032298cb4ffaa0a1093a9a58eef53bd60b785ff9
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-30 12:02:19 +0300

    js

--- JAVASCRIPT/МАССИВЫ/forEach (функция для каждого элемента).js
+++
@@ -1,9 +0,0 @@
-// TODO Метод forEach. Функция для каждого элемента массива (ничего не возвращает)
-
-//* Синтаксис
-arr.forEach(function (item, index, arr) {});
-
-const how = ["быстрее", "выше", "сильнее"];
-how.forEach(function (item) {
-  console.log(item + "."); // быстрее. выше. сильнее.
-});
