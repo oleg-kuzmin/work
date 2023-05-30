92b2f4e618eab87940310c3a1737dcac6bb24669
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-30 11:51:55 +0300

    js

--- JAVASCRIPT/МАССИВЫ/every.js
+++
@@ -1,6 +0,0 @@
-// TODO Метод every. Проверка всех элементов массива.
-const jokes = ['смешная шутка и доля правды', 'не очень смешной анекдот + доля правды', 'доля правды в дурацкой шутке'];
-const allJokesWithTruth = jokes.every(function (joke) {
-  return joke.indexOf('доля правды') > -1;
-});
-console.log(allJokesWithTruth); // true
