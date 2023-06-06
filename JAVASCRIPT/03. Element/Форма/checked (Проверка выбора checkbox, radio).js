e07189b644b5f863df92fab33723ccd3951b748b
Oleg Kuzmin <alex-dust@yandex.ru>
2023-06-06 09:41:08 +0300

    js

--- JAVASCRIPT/03. Element/Форма/checked (Проверка выбора checkbox, radio).js
+++ JAVASCRIPT/03. Element/Форма/checked (Проверка выбора checkbox, radio).js
@@ -2,9 +2,15 @@
 // Это свойство есть только у чекбоксов и радиокнопок. Оно содержит true, если чекбокс отмечен, и false — если нет.
 
 //# пример
+<form name="myForm">
+  <input type="checkbox" name="myCheckbox" />
+  <button class="button">Отправить</button>
+</form>;
+
 const button = document.querySelector('.button');
 const checkbox = document.forms.myForm.elements.myCheckbox;
 
+// по нажатию на кнопку выведем в консоль значения свойства checked чекбокса
 button.addEventListener('click', function (evt) {
   evt.preventDefault();
   console.log(checkbox.checked); // true или false
--- JAVASCRIPT/03. Element/Форма/value (Значение поля ввода).js
+++ JAVASCRIPT/03. Element/Форма/value (Значение поля ввода).js
@@ -1,6 +1,40 @@
 //# .value (Значение поля ввода)
-// Значение поля ввода. Оно есть у всех элементов input.
+// Значение поля ввода. Оно есть у всех элементов input и textarea.
 
-//# пример
-let input = document.querySelector('input');
-console.log(input.value);
+//# input, textarea
+<form name="myForm">
+  <input type="text" name="myInput" />
+  <textarea name="myTextArea"></textarea>
+  <button class="button">Отправить</button>
+</form>;
+
+const button = document.querySelector('.button');
+const form = document.forms.myForm;
+const input = form.elements.myInput;
+const textArea = form.elements.myTextArea;
+
+// по нажатию на кнопку выведем в консоль значения текстовых полей
+button.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  console.log(input.value); // значение input
+  console.log(textArea.value); // значение textArea
+});
+
+//# select
+<form name="myForm">
+  <select name="mySelect">
+    <option value="right">Направо</option>
+    <option value="left">Налево</option>
+    <option value="forward">Прямо</option>
+  </select>
+  <button class="button">Submit</button>
+</form>;
+
+const button2 = document.querySelector('.button');
+const select = document.forms.myForm.elements.mySelect;
+
+// по нажатию на кнопку выведем в консоль значения свойства value выпадающего списка
+button2.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  console.log(select.value); // попадёт то, что выбрано
+});
