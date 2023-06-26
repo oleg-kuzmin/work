7a6272ca95cc98b75dda4b15ab45fcec4cb9eeda
Oleg Kuzmin <alex-dust@yandex.ru>
2023-06-26 17:20:27 +0300

    react

--- REACT NEW/Теория/Начало работы.jsx
+++ REACT NEW/Теория/Начало работы.jsx
@@ -1,9 +1,9 @@
 //# 1. Импорт библиотек
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 //# 2. Создание корневого элемента root
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 //# 3. Запуск рендеринга
 root.render(
@@ -11,3 +11,13 @@ root.render(
     <Index />
   </React.StrictMode>
 );
+
+//# 4. Использование внутри html
+// React
+// https://unpkg.com/react@18/umd/react.development.js
+
+// React DOM
+// https://unpkg.com/react-dom@18/umd/react-dom.development.js
+
+// Babel
+// https://unpkg.com/babel-standalone@6/babel.min.js
