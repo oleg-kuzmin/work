29dedb07009b23c5192cc14e58de7c1e90f00796
Oleg Kuzmin <alex-dust@yandex.ru>
2023-06-26 11:48:09 +0300

    js

--- JAVASCRIPT/00. Теория/Типы данных/Типы данных (Виды).js
+++ JAVASCRIPT/00. Теория/Типы данных/Типы данных (Виды).js
@@ -32,26 +32,6 @@ const five = 5n;
 console.log(seven / five); // 1
 
 
-//# boolean
-/*
-- Логический или булев тип boolean может принимать лишь истинное (true) и ложное (false) значения.
-- Обычно логическим переменным дают названия, начинающиеся с английских глаголов is, should, does, can и подобных.
-*/
-
-//* Первый способ создания — явно указать значение, используя ключевые слова true и false
-const truthyValue1 = true; // Истина
-const falsyValue1 = false; // Ложь
-
-//* Второй способ создания — использовать метод Boolean
-const truthyValue2 = Boolean(1); // «Истина»
-const falsyValue2 = Boolean(''); // «Ложь»
-
-//* Третий способ — использовать выражения, значениями которых будут «истина» или «ложь»
-const truthyValue3 = Boolean(4 < 5); // true
-const anotherTruthy3 = 4 < 5; // true
-const falsyValue3 = Boolean(2 * 2 === 5); // false
-const anotherFalsy3 = 2 * 2 === 5; // false
-
 //# null
 /*
 - Null — это примитивный тип данных, который состоит из единственного значения null. Значение null используют, когда нужно обозначить намеренное отсутствие значения.
