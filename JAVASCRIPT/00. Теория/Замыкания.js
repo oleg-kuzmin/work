9d8d6d6eb8899c757e18256d01f9e2941a5e5570
Oleg Kuzmin <alex-dust@yandex.ru>
2023-06-26 22:07:03 +0300

    js

--- JAVASCRIPT/00. Теория/Замыкания.js
+++ JAVASCRIPT/00. Теория/Замыкания.js
@@ -16,3 +16,7 @@ const anotherCounter = Counter();
 console.log(anotherCounter()); // 1
 console.log(anotherCounter()); // 2
 console.log(anotherCounter()); // 3
+
+//# Операторы короткого замыкания
+// && ||
+// Означает, что js может не выполнять оценку всех операндов в выражении.
