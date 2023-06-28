5fb7bd3eeb82e3f8c7267141f8d2c36e3d6893d5
Oleg Kuzmin <alex-dust@yandex.ru>
2023-06-28 15:00:06 +0300

    react

--- REACT NEW/Теория/Spread.jsx
+++ REACT NEW/Теория/Spread.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       {persons.map(person => {
-        return <Persom {...person} />;
+        return <Person {...person} />;
       })}
     </div>
   );
