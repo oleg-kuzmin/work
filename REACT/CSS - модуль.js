ea77e4f3eb4fd92506bf27f24276528789b86e15
Oleg Kuzmin <alex-dust@yandex.ru>
2023-06-29 13:55:02 +0300

    react

---
+++ REACT/CSS - модуль.js
@@ -0,0 +1,15 @@
+//# components/Info.jsx
+import styles from './Info.module.css';
+
+function Info() {
+  return (
+    <div className={styles.info}>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+//# components/Info.module.css
+// .info {
+//   color: red;
+// }
