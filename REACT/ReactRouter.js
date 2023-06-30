e6f951f481d86eab720d384e4b49a511aa369938
Oleg Kuzmin <alex-dust@yandex.ru>
2023-06-30 13:34:59 +0300

    react

--- REACT/ReactRouter.js
+++ REACT/ReactRouter.js
@@ -3,3 +3,13 @@ react router dom
 react router native
 */
 
+//* npm i react-router-dom
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App"></div>
+    </BrowserRouter>
+  );
+}
