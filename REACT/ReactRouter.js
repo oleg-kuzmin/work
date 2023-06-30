a164c2ddd3212725846ea4cf09582960cc79d7a6
Oleg Kuzmin <alex-dust@yandex.ru>
2023-06-30 15:04:20 +0300

    react

--- REACT/ReactRouter.js
+++ REACT/ReactRouter.js
@@ -15,7 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contacts" element={<Contacts />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
@@ -41,7 +41,7 @@ App
 //* location
 // Все компоненты имеют доступ к объекту location, полученной из ссылки, введенной в строке браузера.
 
-//# Отмена новых запросов на сервер при переходе по страницам
+//# Link (Отмена новых запросов на сервер при переходе по страницам)
 // Вместо внутренних ссылок нужно использовать компонент link, который создает тег <a> с логикой react-router-dom.
 import { Link } from 'react-router-dom';
 
@@ -49,3 +49,38 @@ import { Link } from 'react-router-dom';
   <a href="/">Go to home</a>
   <Link to="/">Go to home</Link>
 </>;
+
+//# Outlet (Использование вложенных маршрутов)
+/*
+Компонент Home является родительским компонентов для About, Contacts, NotFound.
+Содержимое About при переходе на страницу отображается там, где находится Outlet.
+
+*/
+
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="about" element={<About />} />
+            <Route path="contacts" element={<Contacts />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <Menu />
+      <h1>Home</h1>
+      <Outlet />
+    </>
+  );
+}
