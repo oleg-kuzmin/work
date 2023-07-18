e21e8ccebbfff6310d598eb1bf44943b61eb9b50
Oleg Kuzmin <alex-dust@yandex.ru>
2023-07-18 17:38:30 +0300

    react

--- ДОКУМЕНТАЦИЯ/REACT 18/09. JavaScript в JSX с фигурными скобками.jsx
+++ ДОКУМЕНТАЦИЯ/REACT 18/09. JavaScript в JSX с фигурными скобками.jsx
@@ -8,3 +8,20 @@
 - Как использовать объект JavaScript внутри JSX с фигурными скобками
 */
 
+//# Передача строк с кавычками
+// Когда вы хотите передать строковый атрибут в JSX, вы заключаете его в одинарные или двойные кавычки:
+function Avatar() {
+  return <img className="avatar" src="https://i.imgur.com/7vQD0fPs.jpg" alt="Gregorio Y. Zara" />;
+}
+
+// Здесь "https://i.imgur.com/7vQD0fPs.jpg"и "Gregorio Y. Zara"передаются как строки.
+
+// Но что, если вы хотите динамически указать текст src или alt? Вы можете использовать значение из JavaScript, заменив "" на {}:
+function Avatar() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
+  return <img className="avatar" src={avatar} alt={description} />;
+}
+
+// Обратите внимание на разницу между className="avatar", указывающим "avatar"имя класса CSS, которое делает изображение круглым, и src={avatar}считывающим значение переменной JavaScript с именем avatar. Это потому, что фигурные скобки позволяют вам работать с JavaScript прямо в вашей разметке!
+
