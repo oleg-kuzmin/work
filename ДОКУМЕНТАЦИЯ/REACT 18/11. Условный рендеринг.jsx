34c183dbcbc93e027aeee15a51b2cc0140db2e14
Oleg Kuzmin <alex-dust@yandex.ru>
2023-08-02 11:17:46 +0300

    react

--- ДОКУМЕНТАЦИЯ/REACT 18/11. Условный рендеринг.jsx
+++ ДОКУМЕНТАЦИЯ/REACT 18/11. Условный рендеринг.jsx
@@ -36,4 +36,24 @@ if (isPacked) {
 }
 return <li className="item">{name}</li>;
 
+// Если isPacked равен true, этот код возвращает другое дерево JSX. С этим изменением некоторые элементы получают галочку в конце:
 
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li className="item">{name} ✔</li>;
+  }
+  return <li className="item">{name}</li>;
+}
+
+function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
