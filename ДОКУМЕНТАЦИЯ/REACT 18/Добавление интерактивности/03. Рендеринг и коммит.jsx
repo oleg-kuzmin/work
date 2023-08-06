dcb18a6e23d5ddee7b15d330d5aea022b35eb2c1
Oleg Kuzmin <alex-dust@yandex.ru>
2023-08-06 20:37:11 +0300

    react

--- ДОКУМЕНТАЦИЯ/REACT 18/Добавление интерактивности/03. Рендеринг и коммит.jsx
+++ ДОКУМЕНТАЦИЯ/REACT 18/Добавление интерактивности/03. Рендеринг и коммит.jsx
@@ -44,5 +44,22 @@ root.render(<Image />);
 
 // В следующем примере React вызовет Gallery() и Image() несколько раз:
 
+function Gallery() {
+  return (
+    <section>
+      <h1>Inspiring Sculptures</h1>
+      <Image />
+      <Image />
+      <Image />
+    </section>
+  );
+}
 
-
+function Image() {
+  return (
+    <img
+      src="https://i.imgur.com/ZF6s192.jpg"
+      alt="'Floralis Genérica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
+    />
+  );
+}
