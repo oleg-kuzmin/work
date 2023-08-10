df44ad3695bce7849818d96e69d6b120287ce648
Oleg Kuzmin <alex-dust@yandex.ru>
2023-08-10 14:42:43 +0300

    react

--- ДОКУМЕНТАЦИЯ/REACT 18/03. Управление состоянием/04. Сохранение и сброс состояния.jsx
+++ ДОКУМЕНТАЦИЯ/REACT 18/03. Управление состоянием/04. Сохранение и сброс состояния.jsx
@@ -181,3 +181,66 @@ function Counter({ isFancy }) {
 //* Важно
 // Помните, что для React важна позиция в дереве пользовательского интерфейса, а не в разметке JSX! Этот компонент имеет два return с разными <Counter /> тегами JSX внутри и снаружи if:
 
+import { useState } from 'react';
+
+function App() {
+  const [isFancy, setIsFancy] = useState(false);
+  if (isFancy) {
+    return (
+      <div>
+        <Counter isFancy={true} />
+        <label>
+          <input
+            type="checkbox"
+            checked={isFancy}
+            onChange={e => {
+              setIsFancy(e.target.checked)
+            }}
+          />
+          Use fancy styling
+        </label>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Counter isFancy={false} />
+      <label>
+        <input
+          type="checkbox"
+          checked={isFancy}
+          onChange={e => {
+            setIsFancy(e.target.checked)
+          }}
+        />
+        Use fancy styling
+      </label>
+    </div>
+  );
+}
+
+function Counter({ isFancy }) {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
+  if (isFancy) {
+    className += ' fancy';
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>{score}</h1>
+      <button onClick={() => setScore(score + 1)}>
+        Add one
+      </button>
+    </div>
+  );
+}
