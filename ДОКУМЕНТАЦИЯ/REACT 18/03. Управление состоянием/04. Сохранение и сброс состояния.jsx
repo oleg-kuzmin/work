2c960198d3cff6e9f3cf5c8008347a6e4586e395
Oleg Kuzmin <alex-dust@yandex.ru>
2023-08-11 22:28:36 +0300

    react

--- ДОКУМЕНТАЦИЯ/REACT 18/03. Управление состоянием/04. Сохранение и сброс состояния.jsx
+++ ДОКУМЕНТАЦИЯ/REACT 18/03. Управление состоянием/04. Сохранение и сброс состояния.jsx
@@ -245,3 +245,51 @@ function Counter({ isFancy }) {
 
 //# Различные компоненты в одном и том же состоянии сброса положения
 // В этом примере установка флажка заменит <Counter> на <p>:
+
+import { useState } from 'react';
+
+function App() {
+  const [isPaused, setIsPaused] = useState(false);
+  return (
+    <div>
+      {isPaused ? (
+        <p>See you later!</p>
+      ) : (
+        <Counter />
+      )}
+      <label>
+        <input
+          type="checkbox"
+          checked={isPaused}
+          onChange={e => {
+            setIsPaused(e.target.checked)
+          }}
+        />
+        Take a break
+      </label>
+    </div>
+  );
+}
+
+function Counter() {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>{score}</h1>
+      <button onClick={() => setScore(score + 1)}>
+        Add one
+      </button>
+    </div>
+  );
+}
