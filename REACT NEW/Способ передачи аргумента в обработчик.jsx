141aaee01a0cd11bed91dc115d3f2c0b350cc531
Oleg Kuzmin <alex-dust@yandex.ru>
2023-08-30 15:23:28 +0300

    react

---
+++ REACT NEW/Способ передачи аргумента в обработчик.jsx
@@ -0,0 +1,32 @@
+//# Способ передачи аргумента
+//* Короткий вариант (рекомендуется)
+function Component() {
+  const handleClick = argument => {
+    return () => {
+      console.log(argument);
+    };
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick('argument')}></button>
+    </div>
+  );
+}
+
+//* Длинный вариант
+function Component() {
+  const handleClick = argument => {
+    console.log(argument);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          handleClick('argument');
+        }}
+      ></button>
+    </div>
+  );
+}
