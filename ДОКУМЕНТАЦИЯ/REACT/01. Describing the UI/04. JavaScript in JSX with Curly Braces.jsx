086679cc0d96dceaadeb550a6bf7d3a799efdd90
Oleg Kuzmin <alex-dust@yandex.ru>
2023-09-04 12:29:44 +0300

    react

--- ДОКУМЕНТАЦИЯ/REACT/01. Describing the UI/04. JavaScript in JSX with Curly Braces.jsx
+++ ДОКУМЕНТАЦИЯ/REACT/01. Describing the UI/04. JavaScript in JSX with Curly Braces.jsx
@@ -9,3 +9,26 @@
 */
 
 //# Передача строк с кавычками
+// Если вы хотите передать строковый атрибут в JSX, вы помещаете его в одинарные или двойные кавычки:
+
+//* App.js
+function Avatar() {
+  return <img className="avatar" src="https://i.imgur.com/7vQD0fPs.jpg" alt="Gregorio Y. Zara" />;
+}
+
+// Здесь "https://i.imgur.com/7vQD0fPs.jpg"и "Gregorio Y. Zara" передаются как строки.
+
+// Но что, если вы хотите динамически указывать src или alt? Вы можете использовать значение из JavaScript, заменив "кавычки" на {скобки}:
+
+//* App.js
+function Avatar() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
+  return <img className="avatar" src={avatar} alt={description} />;
+}
+
+// Обратите внимание на разницу между className="avatar", который определяет имя класса CSS "avatar", и src={avatar}, который считывает значение переменной JavaScript с именем avatar. Это потому, что фигурные скобки позволяют работать с JavaScript прямо в разметке!
+
+//# Использование фигурных скобок: окно в мир JavaScript
+
+
