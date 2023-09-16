a39b79d321986fc3b04f11a3d186bab2ba60e448
Oleg Kuzmin <alex-dust@yandex.ru>
2023-09-16 15:01:10 +0300

    react

--- REACT/Условный рендеринг.jsx
+++ REACT/Условный рендеринг.jsx
@@ -12,4 +12,20 @@ function Item({ name, isPacked }) {
   return <li className="item">{isPacked ? name + ' ✔' : name}</li>;
 }
 
+//* Логический оператор &&
+function Item({ name, isPacked }) {
+  return (
+    <li className="item">
+      {name} {isPacked && '✔'}
+    </li>
+  );
+}
 
+//* Использование переменной
+function Item({ name, isPacked }) {
+  let itemContent = name;
+  if (isPacked) {
+    itemContent = name + ' ✔';
+  }
+  return <li className="item">{itemContent}</li>;
+}
