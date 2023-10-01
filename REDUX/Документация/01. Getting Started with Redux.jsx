b59c9bb0916288f2d09ec61083b8b0b5d45e678d
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-01 13:33:20 +0300

    redux

--- REDUX/Документация/01. Getting Started with Redux.jsx
+++ REDUX/Документация/01. Getting Started with Redux.jsx
@@ -127,3 +127,50 @@ store.dispatch(incremented()); // {value: 2}
 store.dispatch(decremented()); // {value: 1}
 
 // Redux Toolkit позволяет нам писать более короткую логику, которую легче читать, сохраняя при этом то же поведение Redux и поток данных.
+
+//# Изучение Redux
+// У нас есть множество ресурсов, которые помогут вам изучить Redux.
+
+//* Redux Essentials Tutorial
+// Учебное пособие по Redux Essentials представляет собой руководство «сверху вниз», в котором рассказывается, «как правильно использовать Redux», с использованием наших новейших рекомендуемых API и лучших практик. Мы рекомендуем начать с этого.
+
+//* Redux Fundamentals Tutorial
+// Учебное пособие по основам Redux — это руководство «снизу вверх», в котором объясняется, «как работает Redux» с первых принципов и без каких-либо абстракций, а также почему существуют стандартные шаблоны использования Redux.
+
+//* Learn Modern Redux Livestream
+// Сопровождающий Redux Марк Эриксон появился на шоу «Learn with Jason», чтобы объяснить, как мы рекомендуем использовать Redux сегодня. Шоу включает в себя пример приложения с живым кодом, в котором показано, как использовать Redux Toolkit и перехватчики React-Redux с TypeScript, а также новые API-интерфейсы извлечения данных RTK Query.
+
+// Cсылки на пример исходного кода приложения см. на странице https://www.learnwithjason.dev/let-s-learn-modern-redux
+
+//* Additional Tutorials
+/*
+- Репозиторий Redux содержит несколько примеров проектов, демонстрирующих различные аспекты использования Redux. Почти все примеры имеют соответствующую песочницу CodeSandbox. Это интерактивная версия кода, с которой вы можете играть онлайн. Полный список примеров смотрите на странице Examples (https://redux.js.org/introduction/examples).
+- Бесплатная серия видеороликов "Getting Started with Redux" от создателя Redux Дэна Абрамова и видеокурсы "Building React Applications with Idiomatic Redux" на Egghead.io
+https://app.egghead.io/playlists/fundamentals-of-redux-course-from-dan-abramov-bd5cc867
+https://egghead.io/courses/building-react-applications-with-idiomatic-redux
+- Выступление сопровождающего Redux Марка Эриксона на конференции "Redux Fundamentals" и слайды семинара "Redux Fundamentals"
+https://blog.isquaredsoftware.com/2018/03/presentation-reactathon-redux-fundamentals/
+https://blog.isquaredsoftware.com/2018/06/redux-fundamentals-workshop-slides/
+- Сообщение Дэйва Седдиа "A Complete React Redux Tutorial for Beginners"
+https://daveceddia.com/redux-tutorial/
+*/
+
+//* Other Resources
+/*
+- Часто задаваемые вопросы по Redux отвечают на многие распространенные вопросы о том, как использовать Redux, а в разделе документации "Using Redux" содержится информация об обработке производных данных, тестировании, структурировании логики редуктора и сокращении шаблонов.
+https://redux.js.org/faq
+https://redux.js.org/usage/
+- В серии руководств "Practical Redux" сопровождающего Redux Марка Эриксона демонстрируются реальные промежуточные и продвинутые методы работы с React и Redux (также доступны в виде интерактивного курса на Educative.io).
+https://blog.isquaredsoftware.com/series/practical-redux/
+https://www.educative.io/collection/5687753853370368/5707702298738688
+- В списке ссылок React/Redux разбиты по категориям статьи о работе с reducers и selectors, управлении побочными эффектами, архитектуре Redux и передовых методах и многом другом.
+https://github.com/markerikson/react-redux-links
+https://github.com/markerikson/react-redux-links/blob/master/redux-reducers-selectors.md
+https://github.com/markerikson/react-redux-links/blob/master/redux-side-effects.md
+https://github.com/markerikson/react-redux-links/blob/master/redux-architecture.md
+- Наше сообщество создало тысячи библиотек, дополнений и инструментов, связанных с Redux. На странице документации "Ecosystem" перечислены наши рекомендации, а полный список доступен в каталоге дополнений Redux.
+https://redux.js.org/introduction/ecosystem
+https://github.com/markerikson/redux-ecosystem-links
+*/
+
+//# 
