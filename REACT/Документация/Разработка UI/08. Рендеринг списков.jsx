730091d65013889b3ad276ce0aa7abd6ad4256c7
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-14 13:20:27 +0300

    react

--- REACT/Документация/Разработка UI/08. Рендеринг списков.jsx
+++ REACT/Документация/Разработка UI/08. Рендеринг списков.jsx
@@ -7,3 +7,48 @@
 - Когда и зачем использовать ключи React
 */
 
+//# Рендеринг данных из массивов
+// Допустим, у вас есть список содержимого.
+<ul>
+  <li>Creola Katherine Johnson: mathematician</li>
+  <li>Mario José Molina-Pasquel Henríquez: chemist</li>
+  <li>Mohammad Abdus Salam: physicist</li>
+  <li>Percy Lavon Julian: chemist</li>
+  <li>Subrahmanyan Chandrasekhar: astrophysicist</li>
+</ul>;
+
+// Единственное различие между этими элементами списка — это их содержимое, их данные. При построении интерфейсов часто требуется отображать несколько экземпляров одного и того же компонента, используя разные данные: от списков комментариев до галерей изображений профиля. В таких ситуациях вы можете хранить эти данные в объектах и массивах JavaScript и использовать такие методы, как map() и filter() для вывода списков компонентов на их основе.
+
+// Вот краткий пример того, как сформировать список элементов из массива:
+
+//* 1. Переместите данные в массив:
+const people = [
+  'Creola Katherine Johnson: mathematician',
+  'Mario José Molina-Pasquel Henríquez: chemist',
+  'Mohammad Abdus Salam: physicist',
+  'Percy Lavon Julian: chemist',
+  'Subrahmanyan Chandrasekhar: astrophysicist',
+];
+
+//* 2. Используйте map для создания нового массива узлов jsx из членов people:
+const listItems = people.map(person => <li>{person}</li>);
+
+//* 3. Верните listItems из вашего компонента, обернутого в <ul>:
+function List() {
+  const listItems = people.map(person => <li>{person}</li>);
+  return <ul>{listItems}</ul>;
+}
+
+// Вот результат:
+
+//* App.js
+function List() {
+  const listItems = people.map(person => <li>{person}</li>);
+  return <ul>{listItems}</ul>;
+}
+
+// Обратите внимание, что отображается консольная ошибка: Warning: Each child in a list should have a unique “key” prop.
+
+// Как исправить эту ошибку, вы узнаете позже на этой странице. Прежде чем мы приступим к этому, давайте добавим немного структуры в ваши данные.
+
+//# Фильтрация массивов элементов
