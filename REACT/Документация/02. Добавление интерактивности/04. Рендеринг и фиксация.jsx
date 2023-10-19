27ea0177ed373c310f4c29ee95488c806bd870b3
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-19 11:56:58 +0300

    react

--- REACT/Документация/02. Добавление интерактивности/04. Рендеринг и фиксация.jsx
+++ REACT/Документация/02. Добавление интерактивности/04. Рендеринг и фиксация.jsx
@@ -22,3 +22,25 @@
 */
 
 //# Начальный рендер
+// Когда ваше приложение запускается, вам необходимо вызвать начальный рендеринг. Фреймворки и песочницы иногда скрывают этот код, но он выполняется вызовом createRoot с целевым узлом DOM, а затем вызовом его метода render с вашим компонентом:
+
+//* image.js
+function Image() {
+  return (
+    <img
+      src="https://i.imgur.com/ZF6s192.jpg"
+      alt="'Floralis Genérica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
+    />
+  );
+}
+
+//* index.js
+import Image from './Image.js';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Image />);
+
+// Попробуйте закомментировать вызов root.render() и увидите, как компонент исчезнет!
+
+//# Рендеринг при обновлении состояния
