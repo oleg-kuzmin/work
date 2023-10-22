e247e0214efe78fd44b4804ea29c77683931ba5c
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-22 12:28:32 +0300

    react

--- REACT/Документация/02. Добавление интерактивности/07. Обновление объектов в состоянии.jsx
+++ REACT/Документация/02. Добавление интерактивности/07. Обновление объектов в состоянии.jsx
@@ -488,3 +488,79 @@ updatePerson(draft => {
 //* Как работает Immer?
 // "Черновик", предоставляемый Immer, является особым типом объекта, называемым Proxy, который "записывает" то, что вы с ним делаете. Именно поэтому вы можете свободно мутировать его сколько угодно! Под капотом Immer определяет, какие части черновика были изменены, и создает совершенно новый объект, содержащий ваши правки.
 //* Как работает Immer?
+
+/* Чтобы попробовать Immer:
+1. Запустите npm install use-immer, чтобы добавить Immer в качестве зависимости.
+2. Затем замените import { useState } из 'react' на import { useImmer } из 'use-immer'.
+*/
+
+// Вот вышеприведенный пример, преобразованный в Immer:
+
+//* App.js
+import { useImmer } from 'use-immer';
+
+function Form() {
+  const [person, updatePerson] = useImmer({
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    },
+  });
+
+  function handleNameChange(e) {
+    updatePerson(draft => {
+      draft.name = e.target.value;
+    });
+  }
+
+  function handleTitleChange(e) {
+    updatePerson(draft => {
+      draft.artwork.title = e.target.value;
+    });
+  }
+
+  function handleCityChange(e) {
+    updatePerson(draft => {
+      draft.artwork.city = e.target.value;
+    });
+  }
+
+  function handleImageChange(e) {
+    updatePerson(draft => {
+      draft.artwork.image = e.target.value;
+    });
+  }
+
+  return (
+    <>
+      <label>
+        Name:
+        <input value={person.name} onChange={handleNameChange} />
+      </label>
+      <label>
+        Title:
+        <input value={person.artwork.title} onChange={handleTitleChange} />
+      </label>
+      <label>
+        City:
+        <input value={person.artwork.city} onChange={handleCityChange} />
+      </label>
+      <label>
+        Image:
+        <input value={person.artwork.image} onChange={handleImageChange} />
+      </label>
+      <p>
+        <i>{person.artwork.title}</i>
+        {' by '}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
+      </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
+    </>
+  );
+}
+
+
