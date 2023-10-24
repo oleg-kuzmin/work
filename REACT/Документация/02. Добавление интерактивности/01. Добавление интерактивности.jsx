f2bfb5469eb8572ef911e218150ae6717a037a40
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-24 11:21:50 +0300

    react

--- REACT/Документация/02. Добавление интерактивности/01. Добавление интерактивности.jsx
+++ REACT/Документация/02. Добавление интерактивности/01. Добавление интерактивности.jsx
@@ -143,4 +143,34 @@ function Form() {
   );
 }
 
+//# Постановка в очередь серии обновлений состояния
+// Этот компонент имеет ошибку: нажатие на "+3" увеличивает счет только один раз.
+
+//* App.js
+import { useState } from 'react';
+
+function Counter() {
+  const [score, setScore] = useState(0);
+
+  function increment() {
+    setScore(score + 1);
+  }
+
+  return (
+    <>
+      <button onClick={() => increment()}>+1</button>
+      <button
+        onClick={() => {
+          increment();
+          increment();
+          increment();
+        }}
+      >
+        +3
+      </button>
+      <h1>Score: {score}</h1>
+    </>
+  );
+}
+
 
