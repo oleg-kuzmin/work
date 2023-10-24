3c021933f634246784379e42c4f5dfb2e9ef9d29
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-24 11:24:11 +0300

    react

--- REACT/Документация/02. Добавление интерактивности/01. Добавление интерактивности.jsx
+++ REACT/Документация/02. Добавление интерактивности/01. Добавление интерактивности.jsx
@@ -366,3 +366,60 @@ function Form() {
   );
 }
 
+//# Обновление массивов в состоянии
+// Массивы - это еще один тип изменяемых объектов JavaScript, которые вы можете хранить в состоянии и должны рассматривать как доступные только для чтения. Как и в случае с объектами, когда вы хотите обновить массив, хранящийся в состоянии, вам нужно создать новый массив (или сделать копию существующего), а затем установить состояние для использования нового массива:
+
+//* App.js
+import { useState } from 'react';
+
+let nextId = 3;
+const initialList = [
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
+];
+
+function BucketList() {
+  const [list, setList] = useState(initialList);
+
+  function handleToggle(artworkId, nextSeen) {
+    setList(
+      list.map(artwork => {
+        if (artwork.id === artworkId) {
+          return { ...artwork, seen: nextSeen };
+        } else {
+          return artwork;
+        }
+      })
+    );
+  }
+
+  return (
+    <>
+      <h1>Art Bucket List</h1>
+      <h2>My list of art to see:</h2>
+      <ItemList artworks={list} onToggle={handleToggle} />
+    </>
+  );
+}
+
+function ItemList({ artworks, onToggle }) {
+  return (
+    <ul>
+      {artworks.map(artwork => (
+        <li key={artwork.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={artwork.seen}
+              onChange={e => {
+                onToggle(artwork.id, e.target.checked);
+              }}
+            />
+            {artwork.title}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+}
