48a7ece90d179a9e4b62da331416591563c1872e
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-24 11:19:04 +0300

    react

--- REACT/Документация/02. Добавление интерактивности/01. Добавление интерактивности.jsx
+++ REACT/Документация/02. Добавление интерактивности/01. Добавление интерактивности.jsx
@@ -42,4 +42,57 @@ function Button({ onClick, children }) {
 const [index, setIndex] = useState(0);
 const [showMore, setShowMore] = useState(false);
 
+// Вот как галерея изображений использует и обновляет состояние по щелчку мыши:
+
+//* data.js
+const sculptureList = [
+  {
+    name: 'Homenaje a la Neurocirugía',
+    artist: 'Marta Colvin Andrade',
+    description:
+      'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+    url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+    alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.',
+  },
+];
+
+//* App.js
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < sculptureList.length - 1;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>{showMore ? 'Hide' : 'Show'} details</button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
+  );
+}
+
 
