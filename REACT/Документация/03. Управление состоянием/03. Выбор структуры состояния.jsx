d3e8911237be82f89d6b21620a31bf1698be0467
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-27 13:01:51 +0300

    react

--- REACT/Документация/03. Управление состоянием/03. Выбор структуры состояния.jsx
+++ REACT/Документация/03. Управление состоянием/03. Выбор структуры состояния.jsx
@@ -647,3 +647,75 @@ function PlaceTree({ id, parentId, placesById, onComplete }) {
     </li>
   );
 }
+
+// Вкладывать состояние можно сколько угодно, но если сделать его "плоским", это решит множество проблем. Это облегчает обновление состояния и помогает избежать дублирования в различных частях вложенного объекта.
+
+//* Улучшение использования памяти
+// В идеале, для улучшения использования памяти вы также должны удалить удаленные элементы (и их детей!) из объекта "table". В данной версии это сделано. Она также использует Immer, чтобы сделать логику обновления более лаконичной.
+
+//* App.js
+import { useImmer } from 'use-immer';
+import { initialTravelPlan } from './places.js';
+
+function TravelPlan() {
+  const [plan, updatePlan] = useImmer(initialTravelPlan);
+
+  function handleComplete(parentId, childId) {
+    updatePlan(draft => {
+      // Remove from the parent place's child IDs.
+      const parent = draft[parentId];
+      parent.childIds = parent.childIds.filter(id => id !== childId);
+
+      // Forget this place and all its subtree.
+      deleteAllChildren(childId);
+      function deleteAllChildren(id) {
+        const place = draft[id];
+        place.childIds.forEach(deleteAllChildren);
+        delete draft[id];
+      }
+    });
+  }
+
+  const root = plan[0];
+  const planetIds = root.childIds;
+  return (
+    <>
+      <h2>Places to visit</h2>
+      <ol>
+        {planetIds.map(id => (
+          <PlaceTree key={id} id={id} parentId={0} placesById={plan} onComplete={handleComplete} />
+        ))}
+      </ol>
+    </>
+  );
+}
+
+function PlaceTree({ id, parentId, placesById, onComplete }) {
+  const place = placesById[id];
+  const childIds = place.childIds;
+  return (
+    <li>
+      {place.title}
+      <button
+        onClick={() => {
+          onComplete(parentId, id);
+        }}
+      >
+        Complete
+      </button>
+      {childIds.length > 0 && (
+        <ol>
+          {childIds.map(childId => (
+            <PlaceTree key={childId} id={childId} parentId={id} placesById={placesById} onComplete={onComplete} />
+          ))}
+        </ol>
+      )}
+    </li>
+  );
+}
+
+//* Улучшение использования памяти
+
+// Иногда вложенность состояния можно уменьшить, переместив часть вложенного состояния в дочерние компоненты. Это хорошо работает для эфемерного состояния пользовательского интерфейса, которое не нужно хранить, например, наведение курсора на элемент.
+
+
