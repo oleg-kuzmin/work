530c8c0422a3133f68e75615d1953bd417928f48
Oleg Kuzmin <alex-dust@yandex.ru>
2023-10-30 11:28:30 +0300

    react

--- REACT/Документация/03. Управление состоянием/04. Совместное использование состояния между компонентами.jsx
+++ REACT/Документация/03. Управление состоянием/04. Совместное использование состояния между компонентами.jsx
@@ -83,4 +83,36 @@ Accordion (ближайший общий родитель)
   Panel
 */
 
+// В данном примере это компонент Accordion. Поскольку он находится над обеими панелями и может управлять их пропсами, он станет "источником истины" для того, какая панель в данный момент активна. Заставьте компонент Accordion передавать жестко закодированное значение isActive (например, true) обеим панелям:
+
+//* App.js
+import { useState } from 'react';
+
+function Accordion() {
+  return (
+    <>
+      <h2>Almaty, Kazakhstan</h2>
+      <Panel title="About" isActive={true}>
+        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital
+        city.
+      </Panel>
+      <Panel title="Etymology" isActive={true}>
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full
+        of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the
+        wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic
+        apple.
+      </Panel>
+    </>
+  );
+}
+
+function Panel({ title, children, isActive }) {
+  return (
+    <section className="panel">
+      <h3>{title}</h3>
+      {isActive ? <p>{children}</p> : <button onClick={() => setIsActive(true)}>Show</button>}
+    </section>
+  );
+}
+
 
