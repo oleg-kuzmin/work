4f576ff986a3f0c7f9ba671fc55f47f3f3cdd04a
Oleg Kuzmin <alex-dust@yandex.ru>
2023-11-08 12:07:33 +0300

    react

--- REACT/Документация/03. Управление состоянием/07. Глубокая передача данных с помощью контекста.jsx
+++ REACT/Документация/03. Управление состоянием/07. Глубокая передача данных с помощью контекста.jsx
@@ -14,3 +14,52 @@
 // Но передача пропсов может стать многословной и неудобной, если вам нужно передать какой-то пропс глубоко в дереве, или если многим компонентам нужен один и тот же пропс. Ближайший общий предок может быть далеко от компонентов, которым нужны данные, а поднятие состояния вверх так высоко может привести к ситуации, называемой "prop drilling".
 
 // Было бы здорово, если бы существовал способ "телепортировать" данные к тем компонентам в дереве, которым они нужны, без передачи пропсов. С функцией контекста React это возможно!
+
+//# Контекст: альтернатива передаче props
+// Контекст позволяет родительскому компоненту предоставлять данные всему дереву под ним. Существует множество вариантов использования контекста. Вот один из примеров. Рассмотрим компонент Heading, который принимает level для своего размера:
+
+//* App.js
+import Heading from './Heading.js';
+import Section from './Section.js';
+function Page() {
+  return (
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Heading level={2}>Heading</Heading>
+      <Heading level={3}>Sub-heading</Heading>
+      <Heading level={4}>Sub-sub-heading</Heading>
+      <Heading level={5}>Sub-sub-sub-heading</Heading>
+      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+    </Section>
+  );
+}
+
+//* Section.js
+function Section({ children }) {
+  return <section className="section">{children}</section>;
+}
+
+//* Heading.js
+function Heading({ level, children }) {
+  switch (level) {
+    case 1:
+      return <h1>{children}</h1>;
+    case 2:
+      return <h2>{children}</h2>;
+    case 3:
+      return <h3>{children}</h3>;
+    case 4:
+      return <h4>{children}</h4>;
+    case 5:
+      return <h5>{children}</h5>;
+    case 6:
+      return <h6>{children}</h6>;
+    default:
+      throw Error('Unknown level: ' + level);
+  }
+}
+
+// Допустим, вы хотите, чтобы несколько заголовков в одном разделе всегда имели одинаковый размер:
+
+
+
