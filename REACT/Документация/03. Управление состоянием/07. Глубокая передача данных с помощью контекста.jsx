ef55d0460a35de96500e501be3bb75af24f687af
Oleg Kuzmin <alex-dust@yandex.ru>
2023-11-08 12:22:08 +0300

    react

--- REACT/Документация/03. Управление состоянием/07. Глубокая передача данных с помощью контекста.jsx
+++ REACT/Документация/03. Управление состоянием/07. Глубокая передача данных с помощью контекста.jsx
@@ -208,4 +208,66 @@ function Heading({ children }) {
 
 // Напомним, что это разметка, которую вы пытались заставить работать:
 
+//* App.js
+import Heading from './Heading.js';
+import Section from './Section.js';
+
+function Page() {
+  return (
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
+
+//* Section.js
+function Section({ children }) {
+  return <section className="section">{children}</section>;
+}
+
+//* Heading.js
+import { useContext } from 'react';
+import { LevelContext } from './LevelContext.js';
+
+function Heading({ children }) {
+  const level = useContext(LevelContext);
+  switch (level) {
+    case 1:
+      return <h1>{children}</h1>;
+    case 2:
+      return <h2>{children}</h2>;
+    case 3:
+      return <h3>{children}</h3>;
+    case 4:
+      return <h4>{children}</h4>;
+    case 5:
+      return <h5>{children}</h5>;
+    case 6:
+      return <h6>{children}</h6>;
+    default:
+      throw Error('Unknown level: ' + level);
+  }
+}
+
+//* LevelContext.js
+/*
+import { createContext } from 'react';
+export const LevelContext = createContext(1);
+*/
+
 
