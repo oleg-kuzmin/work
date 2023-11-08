9c27bd8498f0ddf2137d4fffac1c020b95ab9757
Oleg Kuzmin <alex-dust@yandex.ru>
2023-11-08 12:14:19 +0300

    react

--- REACT/Документация/03. Управление состоянием/07. Глубокая передача данных с помощью контекста.jsx
+++ REACT/Документация/03. Управление состоянием/07. Глубокая передача данных с помощью контекста.jsx
@@ -113,4 +113,63 @@ function Page() {
 
 // Контекст позволяет родителю - даже очень далекому - предоставлять некоторые данные всему дереву внутри него.
 
+//# Шаг 1: Создание контекста
+// Во-первых, вам нужно создать контекст. Вам нужно будет экспортировать его из файла, чтобы ваши компоненты могли его использовать:
 
+//* App.js
+import Heading from './Heading.js';
+import Section from './Section.js';
+
+function Page() {
+  return (
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Section>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Section>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Section>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
+
+//* Section.js
+function Section({ children }) {
+  return <section className="section">{children}</section>;
+}
+
+//* Heading.js
+function Heading({ level, children }) {
+  switch (level) {
+    case 1:
+      return <h1>{children}</h1>;
+    case 2:
+      return <h2>{children}</h2>;
+    case 3:
+      return <h3>{children}</h3>;
+    case 4:
+      return <h4>{children}</h4>;
+    case 5:
+      return <h5>{children}</h5>;
+    case 6:
+      return <h6>{children}</h6>;
+    default:
+      throw Error('Unknown level: ' + level);
+  }
+}
+
+//* LevelContext.js
+import { createContext } from 'react';
+export const LevelContext = createContext(1);
+
+// Единственным аргументом для createContext является значение по умолчанию. Здесь 1 означает самый большой уровень заголовка, но вы можете передать любое значение (даже объект). Значение значения по умолчанию вы увидите в следующем шаге.
