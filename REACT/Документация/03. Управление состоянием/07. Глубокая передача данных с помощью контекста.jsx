fc7a7893e7d981dd905eb3c491086f478b876b55
Oleg Kuzmin <alex-dust@yandex.ru>
2023-11-08 12:29:29 +0300

    react

--- REACT/Документация/03. Управление состоянием/07. Глубокая передача данных с помощью контекста.jsx
+++ REACT/Документация/03. Управление состоянием/07. Глубокая передача данных с помощью контекста.jsx
@@ -293,4 +293,78 @@ function Section({ level, children }) {
 
 // Это говорит React: "если какой-либо компонент внутри этой <Section> запрашивает LevelContext, дайте ему этот level". Компонент будет использовать значение ближайшего <LevelContext.Provider> в дереве пользовательского интерфейса над ним.
 
+//* App.js
+import Heading from './Heading.js';
+import Section from './Section.js';
+
+function Page() {
+  return (
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
+
+//* Section.js
+import { LevelContext } from './LevelContext.js';
+
+function Section({ level, children }) {
+  return (
+    <section className="section">
+      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+    </section>
+  );
+}
+
+//* Heading.js
+import { useContext } from 'react';
+import { LevelContext } from './LevelContext.js';
+
+function Heading({ children }) {
+  const level = useContext(LevelContext);
+  switch (level) {
+    case 1:
+      return <h1>{children}</h1>;
+    case 2:
+      return <h2>{children}</h2>;
+    case 3:
+      return <h3>{children}</h3>;
+    case 4:
+      return <h4>{children}</h4>;
+    case 5:
+      return <h5>{children}</h5>;
+    case 6:
+      return <h6>{children}</h6>;
+    default:
+      throw Error('Unknown level: ' + level);
+  }
+}
+
+//* LevelContext.js
+/*
+import { createContext } from 'react';
+export const LevelContext = createContext(1);
+*/
+
+/* Это тот же результат, что и в оригинальном коде, но вам не нужно передавать свойство level каждому компоненту Heading! Вместо этого, он "вычисляет" уровень заголовка, спрашивая ближайший Section выше:
+1. Вы передаете пропс level компоненту <Section>.
+2. Section оборачивает свои дочерние элементы в <LevelContext.Provider value={level}>.
+3. Heading запрашивает ближайшее значение LevelContext выше с помощью useContext(LevelContext).
+*/
+
 
