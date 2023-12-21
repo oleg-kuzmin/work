7cf71d0fba91b94d817ec3f584176c2cb37c1add
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-21 15:13:41 +0300

    html

---
+++ HTML NEW/00. HEAD/link/Атрибут href.jsx
@@ -0,0 +1,7 @@
+//# Атрибут href
+// URL-ссылка на внешний файл. Ссылка может быть как полной или абсолютной http://localhost/sitename/css/style.css, так и неполной или относительной /sitename/css/style.css.
+
+//# Пример
+<head>
+  <link href="style.css" rel="stylesheet" />
+</head>;
