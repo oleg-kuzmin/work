6fa0ed27f933a4aa3a7ba42f94837ef6edd79141
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-23 13:56:20 +0300

    html

--- HTML NEW/00. HEAD/head/head (head).jsx
+++ HTML NEW/00. HEAD/head/head (head).jsx
@@ -7,14 +7,12 @@
 */
 
 //# Пример
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Заголовок страницы</title>
-    <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-    <meta name="author" content="Oleg Kuzmin" />
-    <meta property="og:title" content="Заголовок страницы - Мой сайт" />
-  </head>
-</html>;
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Заголовок страницы</title>
+  <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+  <meta name="author" content="Oleg Kuzmin" />
+  <meta property="og:title" content="Заголовок страницы - Мой сайт" />
+</head>;
