c3bed9e97502e7c89cc0c1a2ef0ce05f78eee3ea
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-24 14:34:53 +0300

    html

---
+++ HTML NEW/canvas/Атрибут height.jsx
@@ -0,0 +1,5 @@
+//# Атрибут height
+// Устанавливает высоту <canvas>
+
+//# Пример
+<canvas width="300" height="300"></canvas>;
---
+++ HTML NEW/canvas/Атрибут width.jsx
@@ -0,0 +1,5 @@
+//# Атрибут width
+// Устанавливает ширину <canvas>
+
+//# Пример
+<canvas width="300" height="300"></canvas>;
