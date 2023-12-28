56d72649d50f0094ea5655be8b80e5cbf4a650cc
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-28 18:50:51 +0300

    html

---
+++ HTML NEW/03. IMAGE/area/Атрибут href.jsx
@@ -0,0 +1,12 @@
+//# Атрибут href
+// Адрес ссылки.
+
+//# Пример
+<>
+  <map name="doka">
+    <area shape="rect" coords="47,4,319,64" href="/html/" target="_blank" alt="HTML" />
+    <area shape="rect" coords="46,69,318,129" href="/css/" target="_blank" alt="CSS" />
+    <area shape="rect" coords="46,133,318,193" href="/js/" target="_blank" alt="JS" />
+  </map>
+  <img usemap="#doka" />
+</>;
