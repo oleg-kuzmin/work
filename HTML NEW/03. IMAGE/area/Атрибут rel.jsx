37f67f93d4778cbbb54bdd3fc9a2c04c76131094
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-28 19:06:14 +0300

    html

--- HTML NEW/03. IMAGE/area/Атрибут rel.jsx
+++ HTML NEW/03. IMAGE/area/Атрибут rel.jsx
@@ -5,7 +5,14 @@
 */
 
 //# Пример
-<a rel="noopener noreferrer" target="_self"></a>;
+<>
+  <map name="doka">
+    <area shape="rect" coords="47,4,319,64" href="/html/" target="_blank" alt="HTML" rel="noopener noreferrer" />
+    <area shape="rect" coords="46,69,318,129" href="/css/" target="_blank" alt="CSS" />
+    <area shape="rect" coords="46,133,318,193" href="/js/" target="_blank" alt="JS" />
+  </map>
+  <img usemap="#doka" />
+</>;
 
 //# Значения
 //* rel="noopener noreferrer"
