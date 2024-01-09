2e374dc2cba0d0c0b52af7650d2a1767bf7a2be9
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-09 16:49:38 +0300

    html

---
+++ HTML/TAGS BLOCK/body (ТЕЛО СТРАНИЦЫ)/body (ТЕЛО СТРАНИЦЫ).jsx
@@ -0,0 +1,17 @@
+//# body (ТЕЛО СТРАНИЦЫ) (body: тело)
+/*
+- Элемент <body> содержит весь контент, который появится на странице: текст, изображения, теги, скрипты JavaScript и так далее.
+- В вашем документе может быть только один <body>.
+*/
+
+//# Пример
+// <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body></body>
+</html>;
---
+++ HTML/TAGS BLOCK/html (СОДЕРЖИМОЕ САЙТА)/Атрибут prefix (РАЗМЕТКА OPEN-GRAPH).jsx
@@ -0,0 +1,11 @@
+//# Атрибут prefix (РАЗМЕТКА OPEN-GRAPH) (prefix: префикс)
+// Здесь прописывают open-graph разметку, типа <html lang="ru-RU" prefix="og: [http://ogp.me/ns#](http://ogp.me/ns#)">. Это чтобы при репосте с сайта в VK, Facebook и другие соцсети передавались те заголовки, картинки и описания, которые мы укажем.
+
+//# Пример
+// <!DOCTYPE html>
+<html prefix="og: [http://ogp.me/ns#](http://ogp.me/ns#)" lang="ru">
+  <head>
+    <title>Название документа</title>
+  </head>
+  <body>Содержимое страницы.</body>
+</html>;
