32d7006a51ad41deab5bca091933e5e85c955f50
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-10 11:42:40 +0300

    html

---
+++ HTML/TAGS INTERACTIVE/source (ИСТОЧНИК МУЛЬТИМЕДИА)/Атрибут src (URL АДРЕС).jsx
@@ -0,0 +1,8 @@
+//# Атрибут src (URL АДРЕС)
+// Требуется только для использования внутри тегов <audio> и <video>. В атрибуте указывается путь до медиафайла. Если тег <source> находится внутри <picture>, то атрибут src игнорируется.
+
+//# Пример
+<audio controls>
+  <source src="song.mp3" type="audio/mpeg" />
+  <source src="song.ogg" type="audio/ogg" />
+</audio>;
