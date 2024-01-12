1688fc3f4b9008d932a0f1cf7de73b0f9b6b28f4
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-12 17:10:32 +0300

    next

---
+++ NEXT/Установка.jsx
@@ -0,0 +1,15 @@
+//# Установка
+/*
+- Would you like to use TypeScript?                    / No
+- Would you like to use ESlint?                        / Yes
+- Would you like to use Tailwind CSS?                  / No
+- Would you like to use `src/` directory?              / Yes
+- Would you like to use App Router?                    / Yes
+- Would you like to customize the default import alias / No
+*/
+
+//* npx create-next-app@latest .
+// Установка в текущей директории.
+
+//* npm install --save-dev sass
+// Установка препроцессора SASS.
