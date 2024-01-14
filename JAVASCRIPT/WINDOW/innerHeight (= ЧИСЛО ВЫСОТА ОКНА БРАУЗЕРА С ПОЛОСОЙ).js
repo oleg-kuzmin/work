a2ac4d0f2dd7220ae83c1b2a81a786db4d70aa65
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-14 12:14:48 +0300

    js

--- JAVASCRIPT/WINDOW/innerHeight (= ЧИСЛО ВЫСОТА ОКНА БРАУЗЕРА С ПОЛОСОЙ).js
+++ JAVASCRIPT/WINDOW/innerHeight (= ЧИСЛО ВЫСОТА ОКНА БРАУЗЕРА С ПОЛОСОЙ).js
@@ -1,5 +1,5 @@
-//# window.innerHeight (ВЫСОТА ОКНА БРАУЗЕРА С ПОЛОСОЙ)
+//# window.innerHeight (= ЧИСЛО ВЫСОТА ОКНА БРАУЗЕРА С ПОЛОСОЙ)
 // Покажет полную высоту окна. window.innerWidth/innerHeight включают в себя полосу прокрутки.
 
 //# Пример
-console.log(window.innerHeight);
+window.innerHeight;
---
+++ JAVASCRIPT/WINDOW/innerWidth (= ЧИСЛО ШИРИНА ОКНА БРАУЗЕРА С ПОЛОСОЙ).js
@@ -0,0 +1,5 @@
+//# innerWidth (= ЧИСЛО ШИРИНА ОКНА БРАУЗЕРА С ПОЛОСОЙ)
+// Покажет полную ширину окна. window.innerWidth/innerHeight включают в себя полосу прокрутки.
+
+//# Пример
+window.innerWidth;
