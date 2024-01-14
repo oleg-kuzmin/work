adefddf64c7fb9b54eef03e44dc2fffdf47f4005
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-14 12:11:59 +0300

    js

---
+++ JAVASCRIPT/WINDOW/innerHeight (ВЫСОТА ОКНА БРАУЗЕРА С ПОЛОСОЙ).js
@@ -0,0 +1,5 @@
+//# window.innerHeight (ВЫСОТА ОКНА БРАУЗЕРА С ПОЛОСОЙ)
+// Покажет полную высоту окна. window.innerWidth/innerHeight включают в себя полосу прокрутки.
+
+//# Пример
+console.log(window.innerHeight);
