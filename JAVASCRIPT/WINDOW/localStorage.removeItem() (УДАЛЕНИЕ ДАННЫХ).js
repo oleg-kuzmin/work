353675745b5718aacc5a57cb9875a9fdb882f2b8
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-15 11:44:39 +0300

    js

---
+++ JAVASCRIPT/WINDOW/localStorage.removeItem() (УДАЛЕНИЕ ДАННЫХ).js
@@ -0,0 +1,9 @@
+//# window.localStorage.removeItem() (УДАЛЕНИЕ ДАННЫХ)
+// Удаляет запись из хранилища removeItem('ключ'). Он успешно выполнится даже если указанного ключа не существует в хранилище.
+
+//# Синтаксис
+//* Строка - ключ
+window.localStorage.removeItem('ключ');
+
+//# Пример
+window.localStorage.removeItem('name');
