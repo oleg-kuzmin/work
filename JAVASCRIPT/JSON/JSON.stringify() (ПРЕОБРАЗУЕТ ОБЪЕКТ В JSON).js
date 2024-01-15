1c3b09ec3170c64bf41281580913f703379e7af3
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-15 12:25:37 +0300

    js

---
+++ JAVASCRIPT/JSON/JSON.stringify() (ПРЕОБРАЗУЕТ ОБЪЕКТ В JSON).js
@@ -0,0 +1,32 @@
+//# JSON.stringify() (ПРЕОБРАЗУЕТ ОБЪЕКТ В JSON)
+// Для того что бы превратить данные в JSON-код, используйте метод JSON.stringify(). Первым аргументом метод принимает значение, которое нужно преобразовать.
+
+//# Синтаксис
+//* Объект
+JSON.stringify(object);
+
+/*
+- JSON основан на JavaScript, но является независимой от языка спецификацией для данных и может использоваться почти с любым языком программирования, поэтому он пропускает некоторые специфические значения объектов JavaScript:
+1. Методы объектов (функции) — {greetings() {alert("Hello World!")}};
+2. Ключи со значением undefined — {"value": undefined}.
+*/
+
+//# Пример
+const hero = {
+  nickname: 'BestHealerEver',
+  level: 7,
+  age: 141,
+  race: 'Gnome',
+  isImmortal: false,
+  things: ['sword', 'helmet', 'belt'],
+  money: {
+    gold: 6387,
+    silver: 1264,
+    bronze: 931,
+    diamonds: 2,
+  },
+};
+
+console.log(typeof hero); // object
+console.log(typeof JSON.stringify(hero)); // string
+console.log(JSON.stringify(hero)); // '{"nickname":"BestHealerEver","level":7,"age":141,"race":"Gnome","isImmortal":false,"things":["sword","helmet","belt"],"money":{"gold":6387,"silver":1264,"bronze":931,"diamonds":2}}'
