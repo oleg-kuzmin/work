062b38bf4df6ecee1f2cf498049c43efe45cdd4f
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-15 11:30:56 +0300

    js

--- JAVASCRIPT/WINDOW/localStorage.setItem() (СОХРАНЕНИЕ ДАННЫХ).js
+++ JAVASCRIPT/WINDOW/localStorage.setItem() (СОХРАНЕНИЕ ДАННЫХ).js
@@ -1,1 +1,10 @@
-//# localStorage (ЛОКАЛЬНОЕ ХРАНИЛИЩЕ)
+//# localStorage.setItem() (СОХРАНЕНИЕ ДАННЫХ)
+// Сохраняет данные в формате ключ-значение в объект window.localStorage.
+
+//# Синтаксис
+//* 1. Строка - ключ
+//* 2. Строка - значение
+window.localStorage.setItem('ключ', 'значение');
+
+//# Пример
+window.localStorage.setItem('name', 'Дока Дог');
