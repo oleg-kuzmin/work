d6e7394d9da9b975951c10687e51e595d1749876
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-18 19:12:00 +0300

    js

--- JAVASCRIPT OLD/02. Document/createElement (Создает элемент).js
+++
@@ -1,8 +0,0 @@
-//# document.createElement() (Создает html-элемент)
-// Создает html-элемент.
-
-//# пример
-// метод createElement принимает на вход имя тега в виде строки
-const listItem = document.createElement('li');
-const divElement = document.createElement('div');
-const imageElement = document.createElement('img');
---
+++ JAVASCRIPT/DOCUMENT/createElement() (СОЗДАЕТ ЭЛЕМЕНТ).js
@@ -0,0 +1,9 @@
+//# document.createElement() (СОЗДАЕТ ЭЛЕМЕНТ)
+// Создает html-элемент.
+
+//# Синтаксис
+//* Строка с именем тега HTML
+document.createElement('li');
+
+//# Пример
+const listItem = document.createElement('li');
