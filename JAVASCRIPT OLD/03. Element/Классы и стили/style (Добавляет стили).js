22bb58f3a153f7139e539304e7511b10c954fab9
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-19 11:22:36 +0300

    js

--- JAVASCRIPT OLD/03. Element/Классы и стили/style (Добавляет стили).js
+++
@@ -1,7 +0,0 @@
-//# .style (Добавляет стили)
-// Стили добавляются с помощью свойств. Свойства именуются по аналогии с CSS-свойствами.
-
-//# пример
-element.style.backgroundColor = 'beige'; // CSS-свойство background-color
-element.style.color = 'gray'; // CSS-свойство color
-element.style.marginTop = '20px'; // CSS-свойство margin-top
---
+++ JAVASCRIPT/ELEMENT/style (ДОБАВЛЯЕТ СТИЛИ).js
@@ -0,0 +1,7 @@
+//# element.style (ДОБАВЛЯЕТ СТИЛИ).js
+// Стили добавляются с помощью свойств. Свойства именуются по аналогии с CSS-свойствами, но в CamelCase.
+
+//# Пример
+element.style.backgroundColor = 'beige'; // CSS-свойство background-color
+element.style.color = 'gray'; // CSS-свойство color
+element.style.marginTop = '20px'; // CSS-свойство margin-top
