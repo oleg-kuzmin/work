8a85c08bfe3a448e622d50263e7c70712b21fbe7
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-19 12:19:31 +0300

    js

--- JAVASCRIPT/ELEMENT/checked (ВЫБОР ЧЕКБОКСА, РАДИО = BOOLEAN).js
+++ JAVASCRIPT/ELEMENT/checked (ВЫБОР ЧЕКБОКСА, РАДИО = BOOLEAN).js
@@ -1,7 +1,7 @@
-//# .checked (Проверка выбора checkbox, radio)
+//# element.checked (ВЫБОР ЧЕКБОКСА, РАДИО = BOOLEAN).js
 // Это свойство есть только у чекбоксов и радиокнопок. Оно содержит true, если чекбокс отмечен, и false — если нет.
 
-//# пример
+//# Пример
 <form name="myForm">
   <input type="checkbox" name="myCheckbox" />
   <button class="button">Отправить</button>
@@ -10,7 +10,6 @@
 const button = document.querySelector('.button');
 const checkbox = document.forms.myForm.elements.myCheckbox;
 
-// по нажатию на кнопку выведем в консоль значения свойства checked чекбокса
 button.addEventListener('click', function (evt) {
   evt.preventDefault();
   console.log(checkbox.checked); // true или false
