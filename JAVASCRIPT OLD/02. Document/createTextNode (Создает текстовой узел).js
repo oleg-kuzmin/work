f550620ddb50d5c5f41edb41e0f279bef4b35942
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-21 13:18:45 +0300

    js

--- JAVASCRIPT OLD/02. Document/createTextNode (Создает текстовой узел).js
+++
@@ -1,6 +0,0 @@
-//# document.createTextNode()
-// Создает текстовой узел.
-
-//# пример
-// метод createTextNode принимает строку — текст узла
-const textItem = document.createTextNode('Hello, world');
