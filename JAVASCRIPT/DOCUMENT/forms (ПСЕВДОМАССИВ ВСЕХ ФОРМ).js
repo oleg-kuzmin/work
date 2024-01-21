ea4752e342fcf625e1e51686633e677ce6923907
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-21 13:28:06 +0300

    js

---
+++ JAVASCRIPT/DOCUMENT/forms (ПСЕВДОМАССИВ ВСЕХ ФОРМ).js
@@ -0,0 +1,18 @@
+//# forms (ПСЕВДОМАССИВ ВСЕХ ФОРМ)
+// Получить псевдомассив форм на странице. Свойство только для чтения, напрямую перезаписать его нельзя.
+
+//# Возвращает
+//* NodeList (Псевдомассив-коллекция DOM-элементов).
+
+//# Пример
+//* Псевдомассив форм
+const collection = document.forms;
+
+//* Форма по имени
+const myForm = document.forms.name;
+
+//* Псевдомассив элементов формы
+const elements = document.forms.name.elements;
+
+//* Элемент по имени
+const element = document.forms.name.elements.name;
