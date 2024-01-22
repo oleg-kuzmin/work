5cd954833c95674fc929f1cb67d341964a4a9783
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-22 14:44:53 +0300

    js

---
+++ JAVASCRIPT/ELEMENT/dom (МАНИПУЛЯЦИИ С DOM)/cloneNode() (КЛОНИРОВАНИЕ ЭЛЕМЕНТА).js
@@ -0,0 +1,16 @@
+//# element.cloneNode() (КЛОНИРОВАНИЕ ЭЛЕМЕНТА)
+// Создает копию элемента без обработчиков.
+
+//# Синтаксис
+//* Boolean - с содержимым или нет
+elementCopy.cloneNode(true);
+// Если хотите скопировать элемент вместе с содержимым, передайте true, без — false. Обработчики событий элемента не скопируются. Их придётся добавить заново.
+
+//# Возвращает
+//* Новый DOM-элемент
+
+//# Пример
+const container = document.querySelector('.container');
+const element = document.querySelector('.element');
+const elementCopy = element.cloneNode(true);
+container.append(elementCopy);
