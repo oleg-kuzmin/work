cee148457934f95a4bcf9bc296f5b50829948a25
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-23 17:13:41 +0300

    js

---
+++ JAVASCRIPT/СОБЫТИЯ/mousedown (ЛЮБАЯ КНОПКА МЫШКИ НАЖАТА).js
@@ -0,0 +1,13 @@
+//# mousedown (ЛЮБАЯ КНОПКА МЫШКИ НАЖАТА)
+// Возникает при щелчке мышью, когда кнопка в нижнем положении. События mousedown и mouseup срабатывают на все кнопки мыши: левую, правую и нажатие на колёсико. Они полезны для создания Drag'n'Drop — интерфейса, в котором элемент можно схватить и перетащить на другое место.
+
+//# Пример
+//* onmousedown
+button.onmousedown = function () {
+  console.log('Клик!');
+};
+
+//* addEventListener
+button.addEventListener('mousedown', function () {
+  console.log('Клик!');
+});
