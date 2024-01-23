f45df212ba4b3b2eb9b4139bdbd79aa9472286fb
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-23 12:24:59 +0300

    js

--- JAVASCRIPT/ELEMENT/dom (МАНИПУЛЯЦИИ С DOM)/firstElementChild (СОДЕРЖИТ ПЕРВЫЙ ДОЧЕРНИЙ ЭЛЕМЕНТ).js
+++ JAVASCRIPT/ELEMENT/dom (МАНИПУЛЯЦИИ С DOM)/firstElementChild (СОДЕРЖИТ ПЕРВЫЙ ДОЧЕРНИЙ ЭЛЕМЕНТ).js
@@ -1,9 +1,11 @@
-//# .firstElementChild
+//# element.firstElementChild (СОДЕРЖИТ ПЕРВЫЙ ДОЧЕРНИЙ ЭЛЕМЕНТ)
 /*
 - Свойство firstElementChild позволяет получить первый дочерний элемент.
 - Если у элемента нет дочерних элементов, вернется null. Все эти свойства доступны только для чтения. Перезаписать их не получится.
 */
 
-//# пример
-const body = document.querySelector('body');
-console.log(body.firstElementChild); // <p>Ребёнок раз</p>
+//# Возвращает
+//* DOM-элемент.
+
+//# Пример
+body.firstElementChild;
