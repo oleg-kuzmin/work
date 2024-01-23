ae6a0be53a9e2d745804957292f446401c20dd59
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-23 12:39:42 +0300

    js

--- JAVASCRIPT OLD/03. Element/Поиск/parentElement (Находит родительский элемент).js
+++
@@ -1,6 +0,0 @@
-//# .parentElement (Находит родительский элемент)
-// Получить родительский элемент.
-
-//# пример
-const element = document.querySelector('p');
-console.log(element.parentElement); // body
