e7b78e6f306eac964a33363e68d39b680828b4de
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-23 12:38:25 +0300

    js

--- JAVASCRIPT OLD/03. Element/Поиск/nextElementSibling (Находит следующий соседний элемент).js
+++
@@ -1,9 +0,0 @@
-//# .nextElementSibling (Находит следующий соседний элемент)
-/*
-- Свойство previousElementSibling содержит ссылку на следующий соседний элемент.
-- Если у элемента нет соседа, вернется null. Все эти свойства доступны только для чтения. Перезаписать их не получится.
-*/
-
-//# пример
-const element = document.querySelectorAll('p')[1];
-console.log(element.nextElementSibling); // <p>Ребёнок три</p>
--- JAVASCRIPT OLD/03. Element/Поиск/previousElementSibling (Находит предыдущий соседний элемент).js
+++
@@ -1,9 +0,0 @@
-//# .previousElementSibling (Находит предыдущий соседний элемент)
-/*
-- Свойство previousElementSibling содержит ссылку на предыдущий соседний элемент.
-- Если у элемента нет соседа, вернется null. Все эти свойства доступны только для чтения. Перезаписать их не получится.
-*/
-
-//# пример
-const element = document.querySelectorAll('p')[1];
-console.log(element.previousElementSibling); // <p>Ребёнок раз</p>
