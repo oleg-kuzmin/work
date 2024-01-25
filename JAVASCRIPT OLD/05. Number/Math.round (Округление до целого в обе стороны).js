2e458a0f56686ec997698b43ed49c04a406cfe64
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-25 12:31:19 +0300

    js

--- JAVASCRIPT OLD/05. Number/Math.round (Округление до целого в обе стороны).js
+++
@@ -1,8 +0,0 @@
-//# Math.round() (Округление до целого в обе стороны)
-// Для обычного округления в большую или меньшую сторону.
-
-//# пример
-console.log(Math.round(9.51)); // 10
-console.log(Math.round(15.52)); // 16
-console.log(Math.round(15.49)); // 15
-console.log(Math.round(5.50)); // 6
