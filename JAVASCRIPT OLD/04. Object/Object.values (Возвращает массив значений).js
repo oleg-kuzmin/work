4e7bf55932ec33ce1cdf0f927ecb79711916dba0
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-25 12:21:52 +0300

    js

--- JAVASCRIPT OLD/04. Object/Object.values (Возвращает массив значений).js
+++
@@ -1,9 +0,0 @@
-//# Object.values() (Возвращает массив значений)
-// Возвращает значения всех свойств и методов объекта
-
-//# пример
-const paulMcCartney = {
-  yesterday: 'all my troubles seemed so far away'
-};
-
-console.log( Object.values(paulMcCartney) ); // ["all my troubles seemed so far away"]
