6ae95d17ccae310f105b15a0594e346e4c903fcf
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-25 12:06:55 +0300

    js

---
+++ JAVASCRIPT/OBJECT/Object.entries() (ПРЕОБРАЗУЕТ ОБЪЕКТ В МАССИВ).js
@@ -0,0 +1,19 @@
+//# Object.entries() (ПРЕОБРАЗУЕТ ОБЪЕКТ В МАССИВ)
+// Метод возвращает массив, состоящий из элементов - массивов [ ключ, значение ].
+
+//# Синтаксис
+//* Объект, который должен стать прототипом нового объекта
+Object.entries(object);
+
+//# Возвращает
+//* Массив.
+
+//# Пример
+const person = {
+  name: 'Oleg',
+  isProgrammer: true,
+  cars: ['Lada', 'Porshe'],
+};
+
+console.log(Object.entries(person));
+// [ ['name', 'Oleg'], ['isProgrammer', true], ['cars', ['Lada', 'Porshe'] ] ]
