c3e3f068b922c64759b450fb72c8c817e244ee0a
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-25 11:35:01 +0300

    js

--- JAVASCRIPT OLD/04. Object/new Object (Object, функция-конструктор).js
+++
@@ -1,9 +0,0 @@
-//# new Date()
-
-//* new Object()
-// создаёт пустой экземпляр Object
-const myObject = new Object();
-
-//* new Date({объект})
-// создаёт экземпляр Object с переданными данными
-const book = new Object({ title: 'Война и мир', author: 'Лев Толстой' });
---
+++ JAVASCRIPT/OBJECT/new Object() (КОНСТРУКТОР).js
@@ -0,0 +1,6 @@
+//# new Object() (КОНСТРУКТОР)
+//* создаёт пустой экземпляр класса Object
+const myObject = new Object();
+
+//* создаёт экземпляр класса Object с данными объекта
+const book = new Object({ title: 'Война и мир', author: 'Лев Толстой' });
