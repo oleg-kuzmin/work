a644baecdf2829a1849ad27d4103c638c3f47b8a
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-25 13:56:02 +0300

    js

--- JAVASCRIPT OLD/05. Number/Number.isInteger (Проверяет целое или дробное).js
+++
@@ -1,7 +0,0 @@
-//# Number.isInteger() (Проверяет целое или дробное)
-// Метод Number.isInteger принимает число как аргумент и проверяет, целое оно или дробное.
-
-//# пример
-const eightAndAHalf = 8.5;
-Number.isInteger(eightAndAHalf); // false
-Number.isInteger(Math.floor(eightAndAHalf)); // true
