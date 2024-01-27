d602edc8b910bd944ea43e10c355f6c2b3c04c10
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-27 12:34:40 +0300

    js

---
+++ JAVASCRIPT/ТЕОРИЯ/Преобразование (= BOOLEAN).js
@@ -0,0 +1,24 @@
+//# Преобразование (= BOOLEAN)
+/*
+Главное правило: Все, что не приводится к false, будет true.
+1) false               // false
+2) 0                   // false
+3) -0                  // false
+4) ''                  // false
+5) null                // false
+6) undefined           // false
+7) NaN                 // false
+8) 0n(тип BigInt)      // false
+*/
+
+Boolean('Непустая строка'); // true
+Boolean(' ');               // true
+Boolean('');                // false
+Boolean(1);                 // true
+Boolean(0);                 // false
+Boolean(-0);                // false
+Boolean(NaN);               // false
+Boolean(null);              // false
+Boolean(undefined);         // false
+Boolean({});                // true
+Boolean([]);                // true
---
+++ JAVASCRIPT/ТЕОРИЯ/Преобразование (= СТРОКА).js
@@ -0,0 +1,7 @@
+//# Преобразование (= СТРОКА)
+
+String(2);         // "2"
+String(NaN);       // "NaN"
+String(undefined); // "undefined"
+String(null);      // "null"
+String(true);      // "true"
