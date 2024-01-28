778c0246e845b08f5db56a6e9a555efe87885312
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-28 12:59:00 +0300

    html

---
+++ HTML/TAGS INTERACTIVE/iframe (ВСТРАИВАНИЕ ФРЕЙМА)/Атрибут allowfullscreen (ПОЛНОЭКРАННЫЙ РЕЖИМ).jsx
@@ -0,0 +1,5 @@
+//# Атрибут allowfullscreen (ПОЛНОЭКРАННЫЙ РЕЖИМ)
+// Разрешает полноэкранный режим
+
+//# Пример
+<iframe width="100%" allowfullscreen src="https://music.yandex.ru/iframe/#track/41284288/5400781/"></iframe>;
