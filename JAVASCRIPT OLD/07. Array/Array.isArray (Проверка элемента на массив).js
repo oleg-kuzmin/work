bc8910eb60616bde676e563cc97982dd69ff11e2
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-28 12:47:23 +0300

    js

--- JAVASCRIPT OLD/07. Array/Array.isArray (Проверка элемента на массив).js
+++
@@ -1,9 +0,0 @@
-//# Array.isArray() (Проверка элемента на массив)
-// Принятый в ES2015 метод проверки элемента - является ли элемент массивом.
-
-//# пример
-const arr = [1, 2, 3];
-const obj = {};
-
-console.log(Array.isArray(arr)); // true
-console.log(Array.isArray(obj)); // false
