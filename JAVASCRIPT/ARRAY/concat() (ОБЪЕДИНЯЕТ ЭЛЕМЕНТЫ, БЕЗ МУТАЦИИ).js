ec49e348df2f843e50c076dfa2719f729046b4dd
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-29 11:59:56 +0300

    js

--- JAVASCRIPT/ARRAY/concat() (ОБЪЕДИНЯЕТ ЭЛЕМЕНТЫ, БЕЗ МУТАЦИИ).js
+++ JAVASCRIPT/ARRAY/concat() (ОБЪЕДИНЯЕТ ЭЛЕМЕНТЫ, БЕЗ МУТАЦИИ).js
@@ -1,17 +1,26 @@
-//# .concat() (Объединяет элементы, без мутации)
+//# concat() (ОБЪЕДИНЯЕТ ЭЛЕМЕНТЫ, БЕЗ МУТАЦИИ)
 /*
 - Метод concat не меняет структуру начального массива, а возвращает новый.
+- Все аргументы будут добавлены в новый массив в том же порядке.
 - Изменения в новом массиве не приведут к изменению исходного.
 - spread-оператор решает эту проблему лучше.
 */
 
-//# пример
-//* все аргументы будут добавлены в новый массив в том же порядке
+//# Синтаксис
+//* Элемент (один или несколько)
+array.concat(element);
+
+//# Возвращает
+//* Новый массив
+
+//# Пример
+//* строки
 const toDoList = ['Посадить дерево', 'Построить дом'];
 const toDoListUpdated = toDoList.concat('Вырастить сына');
-console.log(toDoListUpdated); // ["Посадить дерево", "Построить дом", "Вырастить сына"]
+console.log(toDoListUpdated);
+// ["Посадить дерево", "Построить дом", "Вырастить сына"]
 
-//* несколько значений
+//* переменные
 const moscowAttractions = ['Кремль', 'Третьяковская галерея'];
 const spbAttractions = ['Эрмитаж', 'Мариинский театр'];
 const volgogradAttractions = ['Мамаев Курган', 'Родина-мать'];
@@ -20,10 +29,7 @@ console.log(russiaAttractions);
 // ['Кремль', 'Третьяковская галерея', 'Эрмитаж', 'Мариинский театр', 'Мамаев Курган', 'Родина-мать'];
 
 //* spread-оператор
-const watchedMovies = ['Rocky', 'Terminator 2', 'The Matrix']
-const watchedVideos = ['Rick&Morty', 'lofi hip hop radio', ...watchedMovies]
-
-console.log(watchedVideos)
+const watchedMovies = ['Rocky', 'Terminator 2', 'The Matrix'];
+const watchedVideos = ['Rick&Morty', 'lofi hip hop radio', ...watchedMovies];
+console.log(watchedVideos);
 // ['Rick&Morty', 'lofi hip hop radio', 'Rocky', 'Terminator 2', 'The Matrix']
-
-
