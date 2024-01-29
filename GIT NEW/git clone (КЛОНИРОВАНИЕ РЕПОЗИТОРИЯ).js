e8f7ac4dd730bf6e43e1be8afc52a8b8cb4f1eee
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-29 15:26:15 +0300

    git

---
+++ GIT NEW/git clone (КЛОНИРОВАНИЕ РЕПОЗИТОРИЯ).js
@@ -0,0 +1,7 @@
+//# git clone (КЛОНИРОВАНИЕ РЕПОЗИТОРИЯ)
+
+//* git clone https://github.com/oleg-kuzmin/how-to-learn.git
+// Этот вариант возможен только если репозиторий публичный, при push нужен логин и пароль
+
+//* git clone git@github.com:oleg-kuzmin/work.git
+// Клонирование с использованием ключа.
