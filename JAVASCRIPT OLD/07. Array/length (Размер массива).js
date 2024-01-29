86f36a0d0d79bcfb5982aa5d13a3f618b3d59946
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-29 13:38:46 +0300

    js

--- JAVASCRIPT OLD/07. Array/length (Размер массива).js
+++
@@ -1,6 +0,0 @@
-//# length (Размер массива)
-// Массивы хранят свой размер в свойстве length.
-
-//# пример
-const infoArray = ['Россия', 'Москва', 144.5, 'Russian ruble', true];
-console.log(infoArray.length); // 5
