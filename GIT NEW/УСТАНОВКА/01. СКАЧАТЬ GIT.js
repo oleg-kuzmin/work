acfd57987396b0fd0d048e8cb6ad8ec2e80e99aa
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-29 14:46:32 +0300

    git

---
+++ GIT NEW/УСТАНОВКА/01. СКАЧАТЬ GIT.js
@@ -0,0 +1,8 @@
+//# СКАЧАТЬ GIT
+
+//* https://git-scm.com/download/
+
+/*
+- использовать GitBash
+- использовать Git из командной строки» (Use Git from... the Command Prompt)
+*/
---
+++ GIT NEW/УСТАНОВКА/02. НАСТРОЙКА ИМЕНИ И ПОЧТЫ.js
@@ -0,0 +1,10 @@
+//# НАСТРОЙКА ИМЕНИ И ПОЧТЫ
+
+//* git config --global user.name "Oleg Kuzmin"
+// Установка имени.
+
+//* git config --global user.email info@oleg-kuzmin.ru
+// Установка почты.
+
+//* git config --list
+// Проверка всех свойств конфига.
