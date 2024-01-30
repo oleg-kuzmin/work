2f91929d9df93617670a61a8d841b3b62b09fbb9
Oleg Kuzmin <alex-dust@yandex.ru>
2024-01-30 13:36:54 +0300

    js

--- JAVASCRIPT/DATE/ЛОКАЛЬНОЕ ВРЕМЯ/toISOString (ДАТА В ФОРМАТЕ ISO).js
+++ JAVASCRIPT/DATE/ЛОКАЛЬНОЕ ВРЕМЯ/toISOString (ДАТА В ФОРМАТЕ ISO).js
@@ -1,8 +1,13 @@
-//# .toISOString() (Возвращает строку - дату ISO)
+//# toISOString (ДАТА В ФОРМАТЕ ISO)
+// Возвращает дату в формате iso.
 
+//# Синтаксис
 //* без переменной
 new Date().toISOString(); // '2011-01-26T13:51:50.417Z' (string)
 
 //* с переменной
 const currentDate = new Date();
 currentDate.toISOString(); // '2011-01-26T13:51:50.417Z' (string)
+
+//# Возвращает
+//* Строка - дата в формате iso.
