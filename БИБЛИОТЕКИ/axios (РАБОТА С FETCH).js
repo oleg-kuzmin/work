ee60267abb170bc85bd47e4ece5b0cfb9186f983
Oleg Kuzmin <alex-dust@yandex.ru>
2024-02-02 15:33:17 +0300

    js

--- БИБЛИОТЕКИ/axios (РАБОТА С FETCH).js
+++ БИБЛИОТЕКИ/axios (РАБОТА С FETCH).js
@@ -1,2 +1,12 @@
 //# axios (РАБОТА С FETCH)
-//* npm i axios 
+//* npm i axios
+
+//* Импорт в файл
+import axios from 'axios';
+
+//* Использование get-запрос
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(res => {
+  console.log(res.data);
+});
