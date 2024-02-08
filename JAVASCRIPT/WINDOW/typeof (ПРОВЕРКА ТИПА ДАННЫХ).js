84e40dc15bb950e4de0850f62876ed8478b86c49
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-08 12:07:44 +0300

    js

--- JAVASCRIPT/WINDOW/typeof (ПРОВЕРКА ТИПА ДАННЫХ).js
+++ JAVASCRIPT/WINDOW/typeof (ПРОВЕРКА ТИПА ДАННЫХ).js
@@ -4,16 +4,8 @@
 //# Возвращает
 //* Строку с значением типа.
 
-//# Object
-typeof { key: 'value' } === 'object';
-typeof null === 'object'; // официально признанная ошибка
-
-//# Array
-//* Проверять тип array нужно через Array.isArray()
-typeof [1, 2, 3] === 'object'; // нужно проверять через Array.isArray()
-
 //# Number
-//* Проверять тип number нужно через Number.isFinite()
+// Проверять тип number нужно через Number.isFinite()
 typeof 42 === 'number';
 typeof 4.2 === 'number';
 typeof -42 === 'number';
@@ -21,18 +13,30 @@ typeof Infinity === 'number'; // нужно проверять через Number
 typeof -Infinity === 'number'; // нужно проверять через Number.isFinite()
 typeof NaN === 'number'; // нужно проверять через Number.isNaN()
 
+//# BigInt
+typeof 9007199254740991n === 'bigint';
+typeof BigInt(9007199254740991) === 'bigint';
+
 //# String
 typeof '' === 'string';
 typeof 'string' === 'string';
 typeof 'number' === 'string';
 typeof 'boolean' === 'string';
 
-//# Undefined
-typeof undefined === 'undefined';
-
 //# Boolean
 typeof true === 'boolean';
 typeof false === 'boolean';
+
+//# Undefined
+typeof undefined === 'undefined';
+
+//# Object
+typeof { key: 'value' } === 'object';
+typeof null === 'object'; // официально признанная ошибка
+
+//# Array
+// Проверять тип array нужно через Array.isArray()
+typeof [1, 2, 3] === 'object'; // нужно проверять через Array.isArray()
 
 //# Function
 const multiply = () => a * b;
@@ -40,7 +44,3 @@ typeof multiply === 'function';
 
 //# Symbol
 typeof Symbol() === 'symbol';
-
-//# BigInt
-typeof 9007199254740991n === 'bigint';
-typeof BigInt(9007199254740991) === 'bigint';
