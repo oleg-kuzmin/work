4aa90956977be6021c48b675e4acaee1ae30157d
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-08 18:55:08 +0300

    js

--- JAVASCRIPT/ТЕОРИЯ/Преобразование (= ЧИСЛО).js
+++ JAVASCRIPT/ТЕОРИЯ/Преобразование (= ЧИСЛО).js
@@ -19,17 +19,18 @@ Number('2');                    // 2
 Number(null);                   // 0
 Number('счастье не за горами'); // NaN
 Number(undefined);              // NaN
-console.log('' + 1 + 0);        // "10"
-console.log('' - 1 + 0);        // -1
-console.log(true + false);      // 1
-console.log(6 / '3');           // 2
-console.log('2' * '3');         // 6
-console.log(4 + 5 + 'px');      // "9px"
-console.log('$' + 4 + 5);       // "$45"
-console.log('4' - 2);           // 2
-console.log('4px' - 2);         // NaN
-console.log('  -9  ' + 5);      // "  -9  5"
-console.log('  -9  ' - 5);      // -14
-console.log(null + 1);          // 1
-console.log(undefined + 1);     // NaN
-console.log(' \t \n' - 2);      // -2
+
+console.log('' + 1 + 0);        // "10"      (= СТРОКА) (= СТРОКА)
+console.log('' - 1 + 0);        // -1        (= ЧИСЛО)  (= ЧИСЛО)
+console.log(true + false);      // 1         (= ЧИСЛО)
+console.log(6 / '3');           // 2         (= ЧИСЛО)
+console.log('2' * '3');         // 6         (= ЧИСЛО)
+console.log(4 + 5 + 'px');      // "9px"     (= ЧИСЛО)  (= СТРОКА)
+console.log('$' + 4 + 5);       // "$45"     (= СТРОКА) (= СТРОКА)
+console.log('4' - 2);           // 2         (= ЧИСЛО)
+console.log('4px' - 2);         // NaN       (= NaN)
+console.log('  -9  ' + 5);      // "  -9  5" (= СТРОКА)
+console.log('  -9  ' - 5);      // -14       (= ЧИСЛО)
+console.log(null + 1);          // 1         (= ЧИСЛО)
+console.log(undefined + 1);     // NaN       (= NaN)
+console.log(' \t \n' - 2);      // -2        (= ЧИСЛО)
