2e5f0c475b924a2f8032b024f94a32114424067a
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-13 20:56:23 +0300

    ts

---
+++ TYPESCRIPT/(ИСПОЛЬЗОВАНИЕ).tsx
@@ -0,0 +1,14 @@
+//# (ИСПОЛЬЗОВАНИЕ)
+//* Объявление переменной
+// переменная (тип number)
+let a: number = 1;
+
+//* Объявление функции
+// функция с аргументом value (тип number)
+function makeJob(value: number) {}
+
+//* Объявление функции
+// функция с аргументами (тип number) возвращает (тип number)
+function calcSum(a: number, b: number): number {
+  return 1;
+}
