eac44ac50b2e964eff26ee97a5fe42386559162d
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-17 15:06:02 +0300

    react

--- REACT/ООП/setState (СОСТОЯНИЕ).jsx
+++ REACT/ООП/setState (СОСТОЯНИЕ).jsx
@@ -1,4 +1,5 @@
-//# Классовый метод setState
+//# setState (СОСТОЯНИЕ)
+
 //* Создавая метод класса, в котором планируется использовать setState()​, метод можно связать с​ this через bind
 class UserGreeting extends React.Component {
   constructor(props) {
