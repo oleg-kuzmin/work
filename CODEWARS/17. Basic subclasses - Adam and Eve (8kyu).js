b116a0b52aa428e9c9776da0578ba8b39def83bf
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-18 17:56:19 +0300

    js

--- CODEWARS/17. Basic subclasses - Adam and Eve (8kyu).js
+++ CODEWARS/17. Basic subclasses - Adam and Eve (8kyu).js
@@ -5,6 +5,7 @@
 Вы должны выполнять Божью работу. Метод создания должен возвращать массив длиной 2, содержащий объекты (представляющие Адама и Еву). Первый объект массива должен быть экземпляром класса Man. Второй должен быть экземпляром класса Woman. Оба объекта должны быть подклассами Human. Ваша задача — реализовать классы Human, Man и Woman.
 */
 
+//* мое решение
 class Human {
   render() {
     return {};
@@ -28,3 +29,13 @@ class God {
     return [new Man(), new Woman()];
   }
 }
+
+//* лучшее на сайте
+class God {
+  static create() {
+    return [new Man(), new Woman()];
+  }
+}
+class Human {}
+class Man extends Human {}
+class Woman extends Human {}
