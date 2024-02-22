3abe68c13b58135dee7fb12f5490a26bd987f171
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-22 13:53:22 +0300

    react

--- REACT/ООП/state (СОСТОЯНИЕ).jsx
+++ REACT/ООП/state (СОСТОЯНИЕ).jsx
@@ -1,4 +1,9 @@
-//# setState (СОСТОЯНИЕ)
+//# state (СОСТОЯНИЕ)
+// В классовом компоненте состояние хранится в this.state. Как правило this.state это объект.
+
+
+
+
 
 //* Создавая метод класса, в котором планируется использовать setState()​, метод можно связать с​ this через bind
 class UserGreeting extends React.Component {
