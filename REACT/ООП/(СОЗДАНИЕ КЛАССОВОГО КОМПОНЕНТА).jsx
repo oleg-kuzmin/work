bda26a3e83ef04d40bc2d2470330a0995ca7962d
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-22 13:45:50 +0300

    react

--- REACT/ООП/(СОЗДАНИЕ КЛАССОВОГО КОМПОНЕНТА).jsx
+++ REACT/ООП/(СОЗДАНИЕ КЛАССОВОГО КОМПОНЕНТА).jsx
@@ -1,7 +1,17 @@
 //# (СОЗДАНИЕ КЛАССОВОГО КОМПОНЕНТА)
+// По умолчанию конструктор можно не писать (обычно включен в сборку).
+
+import React from 'react';
+
 //* Классовый компонент
 class UserGreeting extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return <h1>Здравствуйте, {this.props.fullName}</h1>;
   }
 }
+
+export default UserGreeting;
