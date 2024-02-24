248f0632ae63f72460e22c9fe379308e52922b3d
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-24 12:45:15 +0300

    js

---
+++ JAVASCRIPT/ELEMENT/(ФОРМА)/focus() (СДЕЛАЕТ ФОКУС НА ЭЛЕМЕНТЕ).js
@@ -0,0 +1,12 @@
+//# focus() (СДЕЛАЕТ ФОКУС НА ЭЛЕМЕНТЕ)
+// Сделает фокус на элементе (например можно использовать при загрузке страницы).
+
+//# Синтаксис
+input.focus();
+
+//# Возвращает
+//* NONE (undefined).
+
+//# Пример
+const inputUserName = document.forms.myForm.userName;
+inputUserName.focus();
