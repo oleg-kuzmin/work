185a62c6e8d7a67ef5757aca99d3b973b580eaf7
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-02-27 15:31:00 +0300

    react

---
+++ REACT/ХУКИ/useState (СОСТОЯНИЕ).jsx
@@ -0,0 +1,12 @@
+//# useState (СОСТОЯНИЕ)
+// Это функция, результатом работы которой будет массив.
+
+//* Импорт
+import { useState } from 'react';
+
+//* Создание (деструктуризация массива)
+function App() {
+  const [] = useState(null);
+
+  return <div></div>;
+}
