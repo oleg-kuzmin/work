884c60013ec25f7420e6128e03667ed493b52f73
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-03 12:29:11 +0300

    redux

---
+++ REDUX/CLASSIC/(СВЯЗЬ С REACT).jsx
@@ -0,0 +1,19 @@
+//# (СВЯЗЬ С REACT)
+//* npm i react-redux
+// Установить пакет react-redux
+
+//* Импорт provider на уровне app и импорт store
+import { Provider } from 'react-redux';
+import { store } from './store'; // export const store = createStore(counter);
+
+//* Обернуть приложение или нужную часть в provider и передать store
+function Home() {
+  return (
+    <Provider store={store}>
+      <main className={styles.main}>
+        <h1>Hello Redux</h1>
+        <Counter />
+      </main>
+    </Provider>
+  );
+}
