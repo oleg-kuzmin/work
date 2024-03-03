e7ea2ddab25d4f436de81d9302dbc759edf6db04
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-03 12:47:39 +0300

    redux

--- REDUX/CLASSIC/useDispatch (ПЕРЕДАТЬ ACTION).jsx
+++ REDUX/CLASSIC/useDispatch (ПЕРЕДАТЬ ACTION).jsx
@@ -1,3 +1,27 @@
 //# useDispatch (ПЕРЕДАТЬ ACTION)
 //* Импорт
 import { useDispatch } from 'react-redux';
+
+//# Синтаксис
+const dispatch = useDispatch();
+
+//# Возвращает
+//* Функцию dispatch.
+
+//# Пример
+import { increment } from './store';
+
+const Counter = () => {
+  const count = useSelector(state => state);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button
+        onClick={() => {
+          dispatch(increment);
+        }}
+      ></button>
+    </div>
+  );
+};
