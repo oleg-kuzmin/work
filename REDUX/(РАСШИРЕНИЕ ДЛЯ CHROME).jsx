91bab1e19b1ccbccd2e339c085e9775a046813b5
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-04 12:41:31 +0300

    redux

--- REDUX/(РАСШИРЕНИЕ ДЛЯ CHROME).jsx
+++ REDUX/(РАСШИРЕНИЕ ДЛЯ CHROME).jsx
@@ -1,4 +1,8 @@
-export const store = createStore(
+//# (РАСШИРЕНИЕ ДЛЯ CHROME)
+// Передать вторым или третьим параметром.
+export const store1 = createStore(todos, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export const store2 = createStore(
   todos,
   [],
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
