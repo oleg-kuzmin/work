2512debf69b2670ef201fbd8b74d8394f084aacf
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-08 13:18:47 +0300

    redux

--- REDUX/CLASSIC/(СОЗДАНИЕ MIDDLEWARE).jsx
+++ REDUX/CLASSIC/(СОЗДАНИЕ MIDDLEWARE).jsx
@@ -1,10 +1,11 @@
 //# (СОЗДАНИЕ MIDDLEWARE)
 
 //* Создание функции middleware
-// Эту сигнатуру ожидает от нас redux чтобы использовать middleware
+// Эту сигнатуру ожидает от нас redux чтобы использовать middleware. По сути это 3 функции. Нам обязательно нужно чтобы в теле был вызов функции next с аргументом action - next(action).
 const myLogger = store => next => action => {
   console.log('случилось событие', action.type);
-  next(action);
+  next(action); // гарантирует что наш reducer отработает action (синхронно),
+  console.log('обновленный state', store.getState());
 };
 
 //* Импорт helper для подключения middleware
