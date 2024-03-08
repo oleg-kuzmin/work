ee6dd9cde15ccc014a648087f9d3772699197c51
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-08 16:59:09 +0300

    redux

--- REDUX/REDUX-THUNK (БИБЛИОТЕКА)/(ИСПОЛЬЗОВАНИЕ).jsx
+++ REDUX/REDUX-THUNK (БИБЛИОТЕКА)/(ИСПОЛЬЗОВАНИЕ).jsx
@@ -2,5 +2,19 @@
 //* Импорт
 import { thunk } from 'redux-thunk';
 
-//* Использование при создании store
+//* Добавить при создании store
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+//* Пример action-creators
+const addUsers = users => ({
+  type: 'ADD_USERS',
+  payload: users,
+});
+
+// thunk
+export const loadUsers = () => dispatch => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => dispatch(addUsers(data)))
+    .catch(err => console.error(err));
+};
