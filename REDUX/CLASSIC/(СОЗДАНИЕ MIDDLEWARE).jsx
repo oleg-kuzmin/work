dc4bb01e1cd291108a2c909698d79542a6da1ad8
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-08 13:13:39 +0300

    redux

--- REDUX/CLASSIC/(СОЗДАНИЕ MIDDLEWARE).jsx
+++ REDUX/CLASSIC/(СОЗДАНИЕ MIDDLEWARE).jsx
@@ -1,6 +1,6 @@
 //# (СОЗДАНИЕ MIDDLEWARE)
 
-//* Создание функции с middleware
+//* Создание функции middleware
 // Эту сигнатуру ожидает от нас redux чтобы использовать middleware
 const myLogger = store => next => action => {
   console.log('случилось событие', action.type);
@@ -8,4 +8,9 @@ const myLogger = store => next => action => {
 };
 
 //* Импорт helper для подключения middleware
+// Далее applyMiddleware принимает наши функции middleware как параметры.
 import { applyMiddleware } from '@reduxjs/toolkit';
+
+//* Передача middleware при создании store
+// Передаем вторым или третьим параметром. Соответственно он всегда идет последним.
+const store = createStore(counter, applyMiddleware(myLogger));
