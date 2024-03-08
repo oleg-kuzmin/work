01eca44c73e77bc3b0b004006d919cf3503a7446
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-08 14:02:54 +0300

    redux

--- REDUX/(РАСШИРЕНИЕ ДЛЯ CHROME).jsx
+++ REDUX/(РАСШИРЕНИЕ ДЛЯ CHROME).jsx
@@ -7,3 +7,20 @@ export const store2 = createStore(
   [],
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+//# Использование с middleware
+import { createStore, applyMiddleware, compose } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+
+const middleWare = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middleWare.push(logger);
+}
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+export const store = createStore(counter, composeEnhancers(applyMiddleware(...middleWare)));
