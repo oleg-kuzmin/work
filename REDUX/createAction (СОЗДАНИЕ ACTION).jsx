061c7c4b5065842b499aa326f674fb5e45c0c563
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-10 13:04:43 +0300

    redux

---
+++ REDUX/createAction (СОЗДАНИЕ ACTION).jsx
@@ -0,0 +1,21 @@
+//# createAction (СОЗДАНИЕ ACTION)
+//* Импорт
+import { createAction } from '@reduxjs/toolkit';
+
+//# Синтаксис
+//* 1. Строка - тип события (action.type)
+export const addTodo = createAction('ADD_TODO');
+
+addTodo; // функция actionCreator
+addTodo.toString(); // вернет 'ADD_TODO'
+addTodo(); // вернет результат вызова функции - объект action {type: 'ADD_TODO', payload: undefined}
+addTodo('value'); // вернет результат вызова функции - объект action {type: 'ADD_TODO', payload: 'value'}
+
+//* 2. Функция подготовки объекта action
+// В качестве параметра принимает payload
+export const addTodo2 = createAction('ADD_TODO', (title) => ({
+  payload: {
+    title,
+    id 
+  }
+}));
