31bdbbc2a4d96d8dddeac8ab44d2383ccbbcb4f2
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-21 19:18:21 +0300

    redux

--- REDUX/03. configureStore (СОЗДАНИЕ ХРАНИЛИЩА).jsx
+++ REDUX/03. configureStore (СОЗДАНИЕ ХРАНИЛИЩА).jsx
@@ -11,7 +11,7 @@ export const store = configureStore({
   // несколько reducer
   reducer: {
     todos: todoReducer,
-    fllters: iltersReducer,
+    fllters: filtersReducer,
   },
 
   // rootReducer, созданный с помощью combineReducers
