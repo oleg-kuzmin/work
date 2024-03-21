5f498870d943a11dcc807adb208574eed215ea84
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-21 19:15:48 +0300

    redux

--- REDUX/combineReducers (ОБЪЕДИНЕНИЕ REDUCER).jsx
+++
@@ -1,9 +0,0 @@
-//# combineReducers (ОБЪЕДИНЕНИЕ REDUCER)
-// Объединяет несколько функций-редюсеров в один.
-
-//# Синтаксис
-//* Объект со свойствами: ключ (название ключа для state): значение (соответствующий редюсер)
-const rootReducer = combineReducers({
-  counter: counterReducer,
-  todos: todosReducer,
-});
