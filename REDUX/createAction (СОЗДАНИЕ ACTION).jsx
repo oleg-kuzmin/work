b99f364064ce054f38f9653eb84d93e12f4fb2d6
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-22 18:19:40 +0300

    redux

---
+++ REDUX/createAction (СОЗДАНИЕ ACTION).jsx
@@ -0,0 +1,44 @@
+//# createAction (СОЗДАНИЕ ACTION)
+// В RTK action формируется автоматически при создании slice.
+const todoSlice = createSlice({
+  reducers: {
+    addTodo(state, action) {
+      state.todos.push({
+        id: new Date().toISOString(),
+        text: action.payload.text,
+        completed: false,
+      });
+    },
+  },
+});
+
+// Экспортируется далее
+export const { addTodo } = todoSlice.actions;
+
+// Используется далее в компонентах через библиотеку react-redux
+dispatch(addTodo({ text: 'Clear cat' }));
+
+//# Использование
+// Но можно создать action отдельно. Ниже пример аналогично slice.
+
+//* Импорт
+import { createAction } from '@reduxjs/toolkit';
+
+//* Создание
+const addTodo = createAction('addTodo', text => ({
+  payload: {
+    id: new Date().toISOString(),
+    text,
+    completed: false,
+  },
+}));
+
+//# Синтаксис
+// createAction принимает аргументы:
+
+//* 1. Строка - тип события (action.type)
+// Данная строка попадет в возвращаемый объект action с ключем type
+
+//* 2. Функция предпоготовки,
+// Принимает один параметр - то, что мы положим при вызове dispatch(addTodo(параметр))
+// Возвращает объект action с ключем payload и значением - объектом с нужными полями.
