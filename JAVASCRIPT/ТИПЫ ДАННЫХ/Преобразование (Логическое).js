b211bcba171f7c9b75874dd43c08201d3363333d
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-24 14:18:39 +0300

    js

---
+++ JAVASCRIPT/ТИПЫ ДАННЫХ/Преобразование (Логическое).js
@@ -0,0 +1,31 @@
+//# Преобразование (Логическое)
+/*
+- Происходит в логических операциях, но также может быть выполнено явно с помощью функции Boolean(value).
+- В круглых скобках условия if любые данные всегда неявно приводятся к логическому типу.
+
+Правила преобразования:
+- Значения, которые интуитивно «пустые», вроде 0, пустой строки, null, undefined и NaN, становятся false:
+1. false               // false
+2. 0                   // false
+3. -0                  // false
+4. ''                  // false
+5. null                // false
+6. undefined           // false
+7. NaN                 // false
+8. 0n(тип BigInt)      // false
+
+- Все остальные значения становятся true.
+*/
+
+//# Пример
+Boolean('Непустая строка'); // true
+Boolean(' ');               // true
+Boolean('');                // false
+Boolean(1);                 // true
+Boolean(0);                 // false
+Boolean(-0);                // false
+Boolean(NaN);               // false
+Boolean(null);              // false
+Boolean(undefined);         // false
+Boolean({});                // true
+Boolean([]);                // true
