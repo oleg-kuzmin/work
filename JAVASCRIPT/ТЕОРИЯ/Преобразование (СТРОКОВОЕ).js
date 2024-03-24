ef24943e9607215c40dbe285b616f1f663f82cdc
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-03-24 14:14:49 +0300

    js

--- JAVASCRIPT/ТЕОРИЯ/Преобразование (СТРОКОВОЕ).js
+++
@@ -1,8 +0,0 @@
-//# Преобразование (= СТРОКА)
-// Явное преобразование типов - глобальный объект String превращает переданный ему аргумент в строку.
-
-String(2);         // "2"
-String(NaN);       // "NaN"
-String(undefined); // "undefined"
-String(null);      // "null"
-String(true);      // "true"
