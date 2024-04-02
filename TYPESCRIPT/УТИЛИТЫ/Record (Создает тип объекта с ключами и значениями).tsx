b80226d8056fc34fbde90464077bcae12b0a16bb
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-04-02 18:18:56 +0300

    ts

---
+++ TYPESCRIPT/УТИЛИТЫ/Record (Создает тип объекта с ключами и значениями).tsx
@@ -0,0 +1,23 @@
+//# Record (Создает тип объекта с ключами и значениями)
+// Создает тип объекта, ключами свойств которого являются Keys, а значениями свойств — Type. Эту утилиту можно использовать для сопоставления свойств одного типа с другим типом.
+
+//# Синтаксис
+//* Record<Keys, Type>
+
+//# Пример
+// Keys
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+// Type
+type CatName = 'miffy' | 'boris' | 'mordred';
+
+// Record<Keys, Type>
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: 'Persian' },
+  boris: { age: 5, breed: 'Maine Coon' },
+  mordred: { age: 16, breed: 'British Shorthair' },
+};
+
+cats.boris; // { age: 5, breed: 'Maine Coon' }
