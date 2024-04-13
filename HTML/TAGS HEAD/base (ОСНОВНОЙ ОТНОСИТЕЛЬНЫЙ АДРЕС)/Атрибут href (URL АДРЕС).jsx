c2882ba3a07dc9146137319606e4757395213671
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-04-13 17:04:28 +0300

    html

--- HTML/TAGS HEAD/base (ОСНОВНОЙ ОТНОСИТЕЛЬНЫЙ АДРЕС)/Атрибут href (URL АДРЕС).jsx
+++
@@ -1,8 +0,0 @@
-//# Атрибут href (URL АДРЕС) (hyper reference: гиперссылка)
-// Значение атрибута определяет нужный основной адрес.
-
-//# Пример
-<head>
-  <title>Дока</title>
-  <base href="https://ex.amp" target="_self" />
-</head>;
