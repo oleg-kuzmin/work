97d04cb118e0a91983613b8b28e5590c93303414
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-04-13 16:36:56 +0300

    html

--- HTML/TAGS HEAD/link (ВНЕШНИЙ ФАЙЛ)/Атрибут href (URL АДРЕС).jsx
+++
@@ -1,7 +0,0 @@
-//# Атрибут href (URL АДРЕС) (hyper reference: гиперссылка)
-// URL-ссылка на внешний файл. Ссылка может быть как полной или абсолютной http://localhost/sitename/css/style.css, так и неполной или относительной /sitename/css/style.css.
-
-//# Пример
-<head>
-  <link href="style.css" rel="stylesheet" />
-</head>;
