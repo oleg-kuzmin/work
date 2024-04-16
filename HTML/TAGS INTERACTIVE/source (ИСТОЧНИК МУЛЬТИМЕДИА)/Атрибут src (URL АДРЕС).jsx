5abfe12fb56f2ae6f0a22621854df8f1e88632d2
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-04-16 13:46:00 +0300

    html

--- HTML/TAGS INTERACTIVE/source (ИСТОЧНИК МУЛЬТИМЕДИА)/Атрибут src (URL АДРЕС).jsx
+++ HTML/TAGS INTERACTIVE/source (ИСТОЧНИК МУЛЬТИМЕДИА)/Атрибут src (URL АДРЕС).jsx
@@ -6,3 +6,29 @@
   <source src="song.mp3" type="audio/mpeg" />
   <source src="song.ogg" type="audio/ogg" />
 </audio>;
+
+//# Пример source
+<audio controls>
+  <source src="audio.mp3" type="audio/mpeg" />
+  <source src="audio.ogg" type="audio/ogg" />
+  <p>
+    Ваш браузер не поддерживает встроенное аудио. Попробуйте
+    <a href="audio.mp3" download>
+      скачать
+    </a>
+    файл.
+  </p>
+</audio>;
+
+//# Пример source
+// Мы также добавили несколько форматов одного аудиофайла. Если браузер не поддерживает формат Opus, он попробует воспроизвести OGG. Если и с ним не получится, то воспроизведёт файл в MP3. Атрибут type поможет браузеру быстрее определить формат файла.
+<audio controls>
+  <source src="audio.opus" type="audio/ogg; codecs=opus" />
+  <source src="audio.ogg" type="audio/ogg; codecs=vorbis" />
+  <source src="audio.mp3" type="audio/mpeg" />
+  Ваш браузер не поддерживает встроенные аудио. Попробуйте
+  <a href="audio.mp3" download>
+    скачать
+  </a>
+  файл.
+</audio>;
