c562c18f9b564691b7a94e8378d5061a5f639724
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-04-17 13:48:03 +0300

    html

--- HTML/TAGS INTERACTIVE/iframe (ВСТРАИВАНИЕ ФРЕЙМА)/Атрибут allowfullscreen (ПОЛНОЭКРАННЫЙ РЕЖИМ).jsx
+++
@@ -1,5 +0,0 @@
-//# Атрибут allowfullscreen (ПОЛНОЭКРАННЫЙ РЕЖИМ)
-// Разрешает полноэкранный режим
-
-//# Пример
-<iframe width="100%" allowfullscreen src="https://music.yandex.ru/iframe/#track/41284288/5400781/"></iframe>;
