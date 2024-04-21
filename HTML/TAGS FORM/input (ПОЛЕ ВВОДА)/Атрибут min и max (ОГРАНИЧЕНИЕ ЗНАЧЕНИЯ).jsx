4c78ef96ab8b9a8889deef21a7d6ff1d1a492254
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-04-21 13:35:59 +0300

    html

--- HTML/TAGS FORM/input (ПОЛЕ ВВОДА)/Атрибут min и max (ОГРАНИЧЕНИЕ ЗНАЧЕНИЯ).jsx
+++
@@ -1,9 +0,0 @@
-//# Атрибут min и max (ОГРАНИЧЕНИЕ ЗНАЧЕНИЯ)
-/*
-- Атрибуты min и max позволяют устанавливать минимальные и максимальные числовые значения.
-- Работает для <input type="number">, <input type="range">
-- А также для даты в формате ГГГГ-ММ-ДД с <input type="date"> и <input type="datetime-local">.
-*/
-
-//# Пример
-<input min="1" max="10" type="number" />;
