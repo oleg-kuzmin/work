212fbdac83cc0f8defa07dff8b7bd0ad75b2eeb9
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-05-19 16:54:44 +0300

    js

--- JAVASCRIPT/РЕГУЛЯРНЫЕ ВЫРАЖЕНИЯ/match() (ПОИСК ВСЕХ СОВПАДЕНИЙ).js
+++
@@ -1,5 +0,0 @@
-//# Метод match()
-// Метод match() используется для поиска всех совпадений регулярного выражения в заданной строке. Метод возвращает массив, содержащий все найденные совпадения.
-let str6 = 'The quick brown fox jumps over the lazy dog.';
-const pattern6 = /the/gi;
-str6.match(pattern6); // ["the", "the"]
