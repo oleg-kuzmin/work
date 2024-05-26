8ee3fefc867b5983ce294b9f70279b9bb28697e8
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-05-26 20:21:00 +0300

    js

--- JAVASCRIPT/WINDOW/localStorage.clear() (ОЧИСТКА ХРАНИЛИЩА).js
+++
@@ -1,8 +0,0 @@
-//# window.localStorage.clear() (ОЧИСТКА ХРАНИЛИЩА)
-// Метод clear() очищает хранилище полностью.
-
-//# Возвращает
-//* NONE (undefined).
-
-//# Синтаксис
-window.localStorage.clear();
