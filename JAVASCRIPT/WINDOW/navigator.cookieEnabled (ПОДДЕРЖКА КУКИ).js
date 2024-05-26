cd6c4717e7f8a9e4f44110121d502d89cab0fc1a
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-05-26 17:52:49 +0300

    js

--- JAVASCRIPT/WINDOW/navigator.cookieEnabled (ПОДДЕРЖКА КУКИ).js
+++
@@ -1,8 +0,0 @@
-//# window.navigator.cookieEnabled (ПОДДЕРЖКА КУКИ)
-// cookieEnabled возвращает true, если браузер пользователя поддерживает куки и они включены, false в противном случае.
-
-//# Возвращает
-//* BOOLEAN-значeние поддержки куки и что они включены.
-
-//# Пример
-navigator.cookieEnabled; // true
