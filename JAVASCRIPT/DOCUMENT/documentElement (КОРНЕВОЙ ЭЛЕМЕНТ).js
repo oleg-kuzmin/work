3f725f494cb51b468e3afbb373f5efc48d020d93
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-05-29 18:27:55 +0300

    js

--- JAVASCRIPT/DOCUMENT/documentElement (КОРНЕВОЙ ЭЛЕМЕНТ).js
+++
@@ -1,8 +0,0 @@
-//# document.documentElement (КОРНЕВОЙ ЭЛЕМЕНТ)
-// document.documentElement - свойство только для чтения, которое возвращает Element, который является коренным элементом документа document (например элемент <html> для HTML документов).
-
-//# Возвращает
-//* Корневой DOM-элемент.
-
-//# Пример
-const html = document.documentElement;
