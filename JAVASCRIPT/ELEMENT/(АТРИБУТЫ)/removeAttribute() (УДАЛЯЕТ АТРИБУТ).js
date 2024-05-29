1fa326de95d7698548b1564a80ddbfa33b1c012a
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-05-29 19:38:15 +0300

    js

--- JAVASCRIPT/ELEMENT/(АТРИБУТЫ)/removeAttribute() (УДАЛЯЕТ АТРИБУТ).js
+++
@@ -1,9 +0,0 @@
-//# element.removeAttribute() (УДАЛЯЕТ АТРИБУТ)
-// Удаляет атрибут у элемента.
-
-//# Синтаксис
-//* Строка - имя атрибута
-element.removeAttribute('disabled');
-
-//# Возвращает
-//* NONE (undefined).
