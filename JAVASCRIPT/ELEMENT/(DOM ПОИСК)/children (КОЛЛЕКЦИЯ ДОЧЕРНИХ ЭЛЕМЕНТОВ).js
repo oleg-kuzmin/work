41314c04dfb5a2ee78df5ffd3c9c636eb33daf26
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-05-30 13:21:31 +0300

    js

--- JAVASCRIPT/ELEMENT/(DOM ПОИСК)/children (КОЛЛЕКЦИЯ ДОЧЕРНИХ ЭЛЕМЕНТОВ).js
+++
@@ -1,9 +0,0 @@
-//# element.children (КОЛЛЕКЦИЯ ДОЧЕРНИХ ЭЛЕМЕНТОВ)
-// Свойство children содержит псевдомассив всех дочерних элементов указанного элемента.
-
-//# Возвращает
-//* HTMLCollection (Динамическая коллекция).
-
-//# пример
-const body = document.querySelector('body');
-console.log(body.children); // HTMLCollection(3) [p, p, p]
