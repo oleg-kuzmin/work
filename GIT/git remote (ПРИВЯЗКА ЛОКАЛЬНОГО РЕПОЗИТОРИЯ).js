7ed51b7fc59a7f002ad62b95a778e08badf46b7c
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-06-07 16:35:13 +0300

    git

--- GIT/git remote (ПРИВЯЗКА ЛОКАЛЬНОГО РЕПОЗИТОРИЯ).js
+++
@@ -1,4 +0,0 @@
-//# git remote (ПРИВЯЗКА ЛОКАЛЬНОГО РЕПОЗИТОРИЯ)
-
-//* git remote add origin https://github.com/oleg-kuzmin/work.git
-// Находясь в папке с локальным репозиторием, привязываем его к удалённому (URL).
