dd160184c6be0c697d2e432e5391c6c1ab9f9aaf
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-06-21 10:17:05 +0300

    delete

--- ИНТЕРЕСНОЕ/JAVASCRIPT/06. Свайпер.js
+++
@@ -1,5 +0,0 @@
-<nav class="swiper">
-  <ul class="swiper-wrapper">
-    <li class="swiper-slide"></li>
-  </ul>
-</nav>;
--- РЕШЕНИЯ/JS/Input - перенос курсора в конец строки.js
+++
@@ -1,5 +0,0 @@
-//# Input - перенос курсора в конец строки
-const input = evt.target;
-input.selectionStart = input.selectionEnd = input.value.length;
-input.scrollLeft = input.scrollWidth;
-input.focus();
