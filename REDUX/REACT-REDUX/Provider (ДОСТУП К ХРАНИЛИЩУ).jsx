70b8f07cb062685dd8633b5340184eac438ac2ae
Oleg Kuzmin <info@oleg-kuzmin.ru>
2024-07-17 11:45:00 +0300

    md

--- REDUX/REACT-REDUX/Provider (ДОСТУП К ХРАНИЛИЩУ).jsx
+++
@@ -1,7 +0,0 @@
-//# Provider (ДОСТУП К ХРАНИЛИЩУ)
-import { Provider } from 'react-redux'; // импорт компонента
-import store from './store'; // импорт хранилища
-
-<Provider store={store}>
-  <App />
-</Provider>;
--- REDUX/REACT-REDUX/useSelector (ПОЛУЧЕНИЕ ДАННЫХ STATE).jsx
+++
@@ -1,7 +0,0 @@
-//# useSelector (ПОЛУЧЕНИЕ ДАННЫХ STATE)
-import { useSelector } from 'react-redux'; // импорт метода
-
-export default function Component() {
-  //* useSelector принимает в качестве аргумента функцию callback - принимающую объект state и возвращающую значение нужного свойства объекта state
-  const todos = useSelector(state => state.todos); // сохранение в переменной полученных данных
-}
